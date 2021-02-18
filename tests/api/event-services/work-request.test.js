755fe38587e07474a63aa39dad2a23adf25011cc
Martin Fosco <martinfosco@Martins-MacBook-Pro.local>
2021-02-18 09:42:22 -0300

    Recover tests

--- tests/api/event-services/work-request.test.js
+++ tests/api/event-services/work-request.test.js
@@ -41,6 +41,94 @@ describe('handleWorkRequest', () => {
     }
   });
 
+  it('Throws when type isnt valid.', async () => {
+    // Initialize with an empty cache so a worker hit will be encountered.
+    CacheSingleton.createMock({});
+    expect.assertions(1);
+
+    const workRequest = {
+      uuid: '12345',
+      socketId: '6789',
+      experimentId: 'my-experiment',
+      timeout: '2099-01-01T00:00:00Z',
+      body: { name: 'GetEmbedding', type: 'invalidType', config: { minimumDistance: 0.3, distanceMetric: 'invalidMetric' } },
+    };
+
+    try {
+      await handleWorkRequest(workRequest, socket);
+    } catch (e) {
+      expect(e.message).toMatch(
+        /^Error: type does not match the pattern/,
+      );
+    }
+  });
+
+  it('Throws when an invalid distanceMetric in getEmbedding is encountered.', async () => {
+    // Initialize with an empty cache so a worker hit will be encountered.
+    CacheSingleton.createMock({});
+    expect.assertions(1);
+
+    const workRequest = {
+      uuid: '12345',
+      socketId: '6789',
+      experimentId: 'my-experiment',
+      timeout: '2099-01-01T00:00:00Z',
+      body: { name: 'GetEmbedding', type: 'umap', config: { minimumDistance: 0.3, distanceMetric: 'invalidMetric' } },
+    };
+
+    try {
+      await handleWorkRequest(workRequest, socket);
+    } catch (e) {
+      expect(e.message).toMatch(
+        /^Error: distanceMetric is not set to an allowed value/,
+      );
+    }
+  });
+
+  it('Throws when an invalid minimumDistance in getEmbedding is encountered.', async () => {
+    // Initialize with an empty cache so a worker hit will be encountered.
+    CacheSingleton.createMock({});
+    expect.assertions(1);
+
+    const workRequest = {
+      uuid: '12345',
+      socketId: '6789',
+      experimentId: 'my-experiment',
+      timeout: '2099-01-01T00:00:00Z',
+      body: { name: 'GetEmbedding', type: 'umap', config: { minimumDistance: -1, distanceMetric: 'euclidean' } },
+    };
+
+    try {
+      await handleWorkRequest(workRequest, socket);
+    } catch (e) {
+      expect(e.message).toMatch(
+        /^Error: minimumDistance must be at least 0/,
+      );
+    }
+  });
+
+  it('Throws when there is a missing config property.', async () => {
+    // Initialize with an empty cache so a worker hit will be encountered.
+    CacheSingleton.createMock({});
+    expect.assertions(1);
+
+    const workRequest = {
+      uuid: '12345',
+      socketId: '6789',
+      experimentId: 'my-experiment',
+      timeout: '2099-01-01T00:00:00Z',
+      body: { name: 'GetEmbedding', type: 'umap', config: { distanceMetric: 'euclidean' } },
+    };
+
+    try {
+      await handleWorkRequest(workRequest, socket);
+    } catch (e) {
+      expect(e.message).toMatch(
+        /^Error: minimumDistance is a required field/,
+      );
+    }
+  });
+
   it('Triggers pagination when pagination is specified and result is cached already.', async () => {
     CacheSingleton.createMock({
       '71d05443e350f1a9633f8c1cc5282b56': { // pragma: allowlist secret
