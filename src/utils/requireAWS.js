fca550651c814f37758cbaf71573c4458957527e
Marcell Pek <marcell.pek@marcellp.net>
2021-02-19 17:54:31 +0000

    fix

--- src/utils/requireAWS.js
+++ src/utils/requireAWS.js
@@ -1,9 +1,10 @@
 /* eslint-disable global-require */
 const AWSXRay = require('aws-xray-sdk');
+const config = require('../config');
 
 let AWS;
 
-if (process.env.NODE_ENV === 'test' || process.env.JEST_WORKER_ID) {
+if (config.clusterEnv === 'test' || config.clusterEnv === 'development' || process.env.JEST_WORKER_ID) {
   AWS = require('aws-sdk');
 } else {
   AWS = AWSXRay.captureAWS(require('aws-sdk'));
