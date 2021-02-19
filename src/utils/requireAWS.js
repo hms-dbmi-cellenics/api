80d7ebacff7bd324b46c658664f6d3933a808b31
Marcell Pek <marcell.pek@marcellp.net>
2021-02-19 18:10:03 +0000

    fix failing test

--- src/utils/requireAWS.js
+++ src/utils/requireAWS.js
@@ -1,10 +1,9 @@
 /* eslint-disable global-require */
 const AWSXRay = require('aws-xray-sdk');
-const config = require('../config');
 
 let AWS;
 
-if (config.clusterEnv === 'test' || config.clusterEnv === 'development' || process.env.JEST_WORKER_ID) {
+if (process.env.NODE_ENV === 'test' || process.env.JEST_WORKER_ID) {
   AWS = require('aws-sdk');
 } else {
   AWS = AWSXRay.captureAWS(require('aws-sdk'));
