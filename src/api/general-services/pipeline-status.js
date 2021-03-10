e54fff730e274d8aaad95490cf4096cdd9c8d48f
Xavier Verges <460418+xverges@users.noreply.github.com>
2021-03-10 20:27:07 +0100

    Report the SM execution status

--- src/api/general-services/pipeline-status.js
+++ src/api/general-services/pipeline-status.js
@@ -1,12 +1,39 @@
+const AWS = require('../../utils/requireAWS');
+const ExperimentService = require('../route-services/experiment');
+const config = require('../../config');
 
 const getStepsFromExecutionHistory = () => [];
 
-const getPipelineStatus = async (/* experimentId */) => {
+const getPipelineStatus = async (experimentId) => {
+  const { executionArn } = await (new ExperimentService()).getPipelineHandle(experimentId);
+
+  const stepFunctions = new AWS.StepFunctions({
+    region: config.awsRegion,
+  });
+  const execution = await stepFunctions.describeExecution({
+    executionArn,
+  }).promise();
+
+  /*
+  const smDescription = await stepFunctions.describeStateMachineForExecution({
+    executionArn,
+  }).promise();
+  const smDefinition = JSON.parse(smDescription.definition);
+  const history = await stepFunctions.getExecutionHistory({
+    executionArn,
+    includeExecutionData: false,
+  }).promise();
+
+  console.log(JSON.stringify(smDefinition, null, 2));
+  console.log(JSON.stringify(execution, null, 2));
+  console.log(JSON.stringify(history, null, 2));
+  */
+
   const response = {
     pipeline: {
-      startDate: null,
-      stopDate: null,
-      status: '',
+      startDate: execution.startDate,
+      stopDate: execution.stopDate,
+      status: execution.status,
       completedSteps: [],
     },
   };
