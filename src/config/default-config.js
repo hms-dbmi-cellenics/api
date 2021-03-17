15db99e2fc3df344548b757ca715f76b3d2aa508
Xavier Verges <460418+xverges@users.noreply.github.com>
2021-03-17 13:04:27 +0100

    Fix config

--- src/config/default-config.js
+++ src/config/default-config.js
@@ -60,7 +60,7 @@ const config = {
     prefix: '/',
   },
   workerInstanceConfigUrl: 'https://raw.githubusercontent.com/biomage-ltd/iac/master/releases/production/worker.yaml',
-  pipelineInstanceConfigUrl: 'http://127.0.0.1:8080/pipeline.yaml',
+  pipelineInstanceConfigUrl: 'https://raw.githubusercontent.com/biomage-ltd/iac/master/releases/staging/xavier-api63-pipeline7-6.yaml',
   cachingEnabled: true,
   corsOriginUrl: 'https://scp.biomage.net',
 };
