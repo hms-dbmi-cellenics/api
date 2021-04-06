1d7f8a0f05255b59e26c497d25790c2d38f8057e
Marcell Pek <marcell.pek@marcellp.net>
2021-04-06 20:14:49 +0100

    fix

--- src/api/general-services/pipeline-manage/constructors/create-new-step.js
+++ src/api/general-services/pipeline-manage/constructors/create-new-step.js
@@ -23,7 +23,7 @@ const createNewStep = (context, step, args) => {
     Type: 'Task',
     Resource: activityArn,
     ResultPath: null,
-    TimeoutSeconds: 300,
+    TimeoutSeconds: 600,
     Parameters: {
       ...task,
       ...perSample ? { 'sampleUuid.$': '$.sampleUuid' } : { sampleUuid: '' },
