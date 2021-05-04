a0764105d229e7153cd0a1b5016eaa06e0b52eff
Anugerah Erlaut <aerlaut@live.com>
2021-05-04 17:49:15 +0700

    refactor task to run

--- src/api/route-services/pipeline-response.js
+++ src/api/route-services/pipeline-response.js
@@ -1,17 +1,22 @@
 const AWSXRay = require('aws-xray-sdk');
-const { v4: uuidv4 } = require('uuid');
 const AWS = require('../../utils/requireAWS');
 const validateRequest = require('../../utils/schema-validator');
 const logger = require('../../utils/logging');
 
 const ExperimentService = require('./experiment');
 const PlotsTablesService = require('./plots-tables');
-const WorkSubmitService = require('../general-services/work-submit');
-
-const pipelineStatus = require('../general-services/pipeline-status');
+const PipelineHook = require('../../utils/hookRunner');
 
 const plotsTableService = new PlotsTablesService();
 const experimentService = new ExperimentService();
+
+const pipelineStatus = require('../general-services/pipeline-status');
+const embeddingWorkRequest = require('../../utils/hooks/embeddingWorkRequest');
+const clusteringWorkRequest = require('../../utils/hooks/clusteringWorkRequest');
+
+const pipelineHook = new PipelineHook();
+pipelineHook.register('configureEmbedding', embeddingWorkRequest);
+pipelineHook.register('configureEmbedding', clusteringWorkRequest);
 
 const pipelineResponse = async (io, message) => {
   await validateRequest(message, 'PipelineResponse.v1.yaml');
@@ -81,52 +86,11 @@ const pipelineResponse = async (io, message) => {
   }
 
   const statusRes = await pipelineStatus(experimentId);
-
-  if (taskName === 'configureEmbedding') {
-    const now = new Date();
-    const addTimeoutSeconds = 60;
-    const timeout = new Date(now.getTime() + addTimeoutSeconds * 1000);
-
-    // Run work request for embedding
-    const embeddingWorkRequest = {
-      experimentId,
-      timeout,
-      uuid: uuidv4(),
-      body: {
-        name: 'GetEmbedding',
-        type: output.config.embeddingSettings.method,
-        config: output.config.embeddingSettings.methodSettings[
-          output.config.embeddingSettings.method
-        ],
-      },
-      PipelineRunETag: statusRes.pipeline.startDate,
-    };
-
-    const embeddingWork = new WorkSubmitService(embeddingWorkRequest);
-    await embeddingWork.submitWork();
-
-    // Run work request for cell clustering
-    const clusteringWorkRequest = {
-      experimentId,
-      timeout,
-      uuid: uuidv4(),
-      body: {
-        name: 'ClusterCells',
-        cellSetName: 'Louvain clusters',
-        type: output.config.clusteringSettings.method,
-        cellSetKey: 'louvain',
-        config: {
-          resolution: output.config.clusteringSettings.methodSettings[
-            output.config.clusteringSettings.method
-          ],
-        },
-      },
-      PipelineRunETag: statusRes.pipeline.startDate,
-    };
-
-    const clusteringWork = new WorkSubmitService(clusteringWorkRequest);
-    await clusteringWork.submitWork();
-  }
+  pipelineHook.run(taskName, {
+    experimentId,
+    output,
+    statusRes,
+  });
 
   // Concatenate into a proper response.
   const response = {
