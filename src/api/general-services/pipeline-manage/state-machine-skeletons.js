7592d0de3e1d7788f647b474ce9af7b966ed2729
Pol Alvarez <pol.avms@gmail.com>
2021-05-13 10:02:00 +0200

    created gem2s endpoint

---
+++ src/api/general-services/pipeline-manage/state-machine-skeletons.js
@@ -0,0 +1,254 @@
+const config = require('../../../config');
+
+
+const pipelineSkeleton = {
+  Comment: `Pipeline for clusterEnv '${config.clusterEnv}'`,
+  StartAt: 'DeleteCompletedPipelineWorker',
+  States: {
+    DeleteCompletedPipelineWorker: {
+      XStepType: 'delete-completed-jobs',
+      Next: 'LaunchNewPipelineWorker',
+      ResultPath: null,
+    },
+    LaunchNewPipelineWorker: {
+      XStepType: 'create-new-job-if-not-exist',
+      Next: 'ClassifierFilterMap',
+      ResultPath: null,
+    },
+    ClassifierFilterMap: {
+      Type: 'Map',
+      Next: 'CellSizeDistributionFilterMap',
+      ResultPath: null,
+      ItemsPath: '$.samples',
+      Iterator: {
+        StartAt: 'ClassifierFilter',
+        States: {
+          ClassifierFilter: {
+            XStepType: 'create-new-step',
+            XConstructorArgs: {
+              perSample: true,
+              taskName: 'classifier',
+            },
+            End: true,
+          },
+        },
+      },
+    },
+    CellSizeDistributionFilterMap: {
+      Type: 'Map',
+      Next: 'MitochondrialContentFilterMap',
+      ResultPath: null,
+      ItemsPath: '$.samples',
+      Iterator: {
+        StartAt: 'CellSizeDistributionFilter',
+        States: {
+          CellSizeDistributionFilter: {
+            XStepType: 'create-new-step',
+            XConstructorArgs: {
+              perSample: true,
+              taskName: 'cellSizeDistribution',
+            },
+            End: true,
+          },
+        },
+      },
+    },
+    MitochondrialContentFilterMap: {
+      Type: 'Map',
+      Next: 'NumGenesVsNumUmisFilterMap',
+      ResultPath: null,
+      ItemsPath: '$.samples',
+      Iterator: {
+        StartAt: 'MitochondrialContentFilter',
+        States: {
+          MitochondrialContentFilter: {
+            XStepType: 'create-new-step',
+            XConstructorArgs: {
+              perSample: true,
+              taskName: 'mitochondrialContent',
+            },
+            End: true,
+          },
+        },
+      },
+    },
+    NumGenesVsNumUmisFilterMap: {
+      Type: 'Map',
+      Next: 'DoubletScoresFilterMap',
+      ResultPath: null,
+      ItemsPath: '$.samples',
+      Iterator: {
+        StartAt: 'NumGenesVsNumUmisFilter',
+        States: {
+          NumGenesVsNumUmisFilter: {
+            XStepType: 'create-new-step',
+            XConstructorArgs: {
+              perSample: true,
+              taskName: 'numGenesVsNumUmis',
+            },
+            End: true,
+          },
+        },
+      },
+    },
+    DoubletScoresFilterMap: {
+      Type: 'Map',
+      Next: 'DataIntegration',
+      ResultPath: null,
+      ItemsPath: '$.samples',
+      Iterator: {
+        StartAt: 'DoubletScoresFilter',
+        States: {
+          DoubletScoresFilter: {
+            XStepType: 'create-new-step',
+            XConstructorArgs: {
+              perSample: true,
+              taskName: 'doubletScores',
+            },
+            End: true,
+          },
+        },
+      },
+    },
+    DataIntegration: {
+      XStepType: 'create-new-step',
+      XConstructorArgs: {
+        perSample: false,
+        taskName: 'dataIntegration',
+      },
+      Next: 'ConfigureEmbedding',
+    },
+    ConfigureEmbedding: {
+      XStepType: 'create-new-step',
+      XConstructorArgs: {
+        perSample: false,
+        taskName: 'configureEmbedding',
+        uploadCountMatrix: true,
+      },
+      Next: 'EndOfPipeline',
+    },
+    EndOfPipeline: {
+      Type: 'Pass',
+      End: true,
+    },
+  },
+};
+
+
+const gem2sSkeleton = {
+  Comment: `Gem2s for clusterEnv '${config.clusterEnv}'`,
+  StartAt: 'DeleteCompletedGem2SWorker',
+  States: {
+    DeleteCompletedGem2SWorker: {
+      XStepType: 'delete-completed-jobs',
+      Next: 'LaunchNewGem2SWorker',
+      ResultPath: null,
+    },
+    LaunchNewGem2SWorker: {
+      XStepType: 'create-new-job-if-not-exist',
+      Next: 'DownloadGem',
+      ResultPath: null,
+    },
+    DownloadGem: {
+      XStepType: 'create-new-step',
+      XConstructorArgs: {
+        taskName: 'downloadGem',
+        samples: [],
+      },
+      Next: 'PreProcessingMap',
+      // ItemsPath: '$.samples',
+      // Iterator: {
+      //   StartAt: 'ClassifierFilter',
+      //   States: {
+      //     ClassifierFilter: {
+      //       XStepType: 'create-new-step',
+      //       XConstructorArgs: {
+      //         perSample: true,
+      //         taskName: 'classifier',
+      //       },
+      //       End: true,
+      //     },
+      //   },
+      // },
+    },
+    PreProcessingMap: {
+      Type: 'Map',
+      Next: 'EmptyDropsMap',
+      ResultPath: null,
+      ItemsPath: '$.samples',
+      Iterator: {
+        StartAt: 'PreprocessingFilter',
+        States: {
+          PreprocessingFilter: {
+            XStepType: 'create-new-step',
+            XConstructorArgs: {
+              perSample: true,
+              taskName: 'preproc',
+            },
+            End: true,
+          },
+        },
+      },
+    },
+    EmptyDropsMap: {
+      Type: 'Map',
+      Next: 'DoubletScoresMap',
+      ResultPath: null,
+      ItemsPath: '$.samples',
+      Iterator: {
+        StartAt: 'EmptyDrops',
+        States: {
+          EmptyDrops: {
+            XStepType: 'create-new-step',
+            XConstructorArgs: {
+              perSample: true,
+              taskName: 'emptyDrops',
+            },
+            End: true,
+          },
+        },
+      },
+    },
+    DoubletScoresMap: {
+      Type: 'Map',
+      Next: 'CreateSeurat',
+      ResultPath: null,
+      ItemsPath: '$.samples',
+      Iterator: {
+        StartAt: 'DoubletScores',
+        States: {
+          DoubletScores: {
+            XStepType: 'create-new-step',
+            XConstructorArgs: {
+              perSample: true,
+              taskName: 'doubletScores',
+            },
+            End: true,
+          },
+        },
+      },
+    },
+    CreateSeurat: {
+      XStepType: 'create-new-step',
+      XConstructorArgs: {
+        perSample: false,
+        taskName: 'createSeurat',
+      },
+      Next: 'PrepareExperiment',
+    },
+    PrepareExperiment: {
+      XStepType: 'create-new-step',
+      XConstructorArgs: {
+        perSample: false,
+        taskName: 'prepareExperiment',
+      },
+      Next: 'EndOfGem2S',
+    },
+    EndOfGem2S: {
+      Type: 'Pass',
+      End: true,
+    },
+  },
+};
+
+module.exports = { pipelineSkeleton, gem2sSkeleton };
---
+++ src/api/routes/gem2s.js
@@ -0,0 +1,54 @@
+const AWSXRay = require('aws-xray-sdk');
+const { createGem2S } = require('../general-services/pipeline-manage');
+const ExperimentService = require('../route-services/experiment');
+const getBackendStatus = require('../general-services/backend-status');
+const pipelineResponse = require('../route-services/pipeline-response');
+const parseSNSMessage = require('../../utils/parse-sns-message');
+const logger = require('../../utils/logging');
+
+module.exports = {
+  'gem2s#get': (req, res, next) => {
+    getBackendStatus(req.params.experimentId)
+      .then((data) => res.json(data))
+      .catch(next);
+  },
+
+  'gem2s#create': (req, res, next) => {
+    createGem2S(req.params.experimentId)
+      .then((data) => {
+        const experimentService = new ExperimentService();
+        experimentService.savePipelineHandle(req.params.experimentId, data)
+          .then(() => res.json(data));
+      })
+      .catch(next);
+  },
+
+  'gem2s#response': async (req, res) => {
+    let result;
+
+    try {
+      result = await parseSNSMessage(req);
+    } catch (e) {
+      logger.error('Parsing initial SNS message failed:', e);
+      AWSXRay.getSegment().addError(e);
+      res.status(200).send('nok');
+      return;
+    }
+
+    const { io, parsedMessage } = result;
+
+    try {
+      await pipelineResponse(io, parsedMessage);
+    } catch (e) {
+      logger.error(
+        'Pipeline response handler failed with error: ', e,
+      );
+
+      AWSXRay.getSegment().addError(e);
+      res.status(200).send('nok');
+      return;
+    }
+
+    res.status(200).send('ok');
+  },
+};
