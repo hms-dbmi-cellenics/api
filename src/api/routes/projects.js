39dc54fd7fc1792732df180fc483558bc22b0947
Anugerah Erlaut <aerlaut@live.com>
2021-05-24 16:44:37 +0700

    require auth for saving and loading projects

--- src/api/routes/projects.js
+++ src/api/routes/projects.js
@@ -1,21 +1,29 @@
 const ProjectsService = require('../route-services/projects');
 
+const { expressAuthenticationOnlyMiddleware } = require('../../utils/authMiddlewares');
+
 const projectsService = new ProjectsService();
 
 module.exports = {
-  'projects#update': (req, res, next) => {
-    projectsService.updateProject(req.params.projectUuid, req.body)
-      .then((data) => res.json(data))
-      .catch(next);
-  },
-  'projects#delete': (req, res, next) => {
-    projectsService.deleteProject(req.params.projectUuid)
-      .then((data) => res.json(data))
-      .catch(next);
-  },
+  'projects#update': [
+    expressAuthenticationOnlyMiddleware,
+    (req, res, next) => {
+      projectsService.updateProject(req.params.projectUuid, req.body)
+        .then((data) => res.json(data))
+        .catch(next);
+    }],
+  'projects#delete': [
+    expressAuthenticationOnlyMiddleware,
+    (req, res, next) => {
+      projectsService.deleteProject(req.params.projectUuid)
+        .then((data) => res.json(data))
+        .catch(next);
+    }],
 
-  'projects#get': (req, res, next) => {
-    projectsService.getProjects().then((response) => res.json(response)).catch(next);
-  },
-
+  'projects#get': [
+    expressAuthenticationOnlyMiddleware,
+    (req, res, next) => {
+      projectsService.getProjects().then((response) => res.json(response)).catch(next);
+    },
+  ],
 };
