8962ef33c04125fd0394d26a0580b104292b2a24
Xavier Verges <460418+xverges@users.noreply.github.com>
2021-05-26 06:55:10 +0200

    Restore change lost on merge

--- src/api/routes/projects.js
+++ src/api/routes/projects.js
@@ -25,7 +25,7 @@ module.exports = {
   'projects#get': [
     expressAuthenticationOnlyMiddleware,
     (req, res, next) => {
-      projectsService.getProjects().then((response) => res.json(response)).catch(next);
+      projectsService.getProjects(req.user).then((response) => res.json(response)).catch(next);
     },
   ],
   'projects#getExperiments': [
