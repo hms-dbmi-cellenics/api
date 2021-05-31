eabc1986c334719dca2d163ce67ca9bf3e194d2b
Anugerah Erlaut <aerlaut@live.com>
2021-05-31 15:41:33 +0700

    fix nonexistent property access

--- src/utils/cache-request.js
+++ src/utils/cache-request.js
@@ -38,7 +38,7 @@ const cacheSetResponse = async (data, ttl = 129600) => {
     experimentId: data.request.experimentId,
     body: data.request.body,
     sandboxId,
-    PipelineRunETag: data.PipelineRunETag,
+    PipelineRunETag: data.request.PipelineRunETag,
   });
 
   logger.log(`Putting data in cache under key ${key}`);
