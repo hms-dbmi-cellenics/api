9ee09b7ff9ace472a6e972b9ac18614489fb8c13
Anugerah Erlaut <anugerah@biomage.net>
2021-07-19 18:55:56 +0700

    [BIOMAGE-1208] - Update defaultFilterSettings when QC pipeline is run with auto (#181)
    * Add code coverage github action (#173)
    * Add violin plot to API schema (#180)
    * pass default filter settings to front end
    * if not auto use previous default settings
    
    Co-authored-by: Martin Fosco <cosa65@users.noreply.github.com>
    Co-authored-by: Martin Fosco <martinfosco@Martins-MacBook-Pro.local>
    Co-authored-by: Marcell Pék <marcell@biomage.net>

--- src/api/route-services/pipeline-response.js
+++ src/api/route-services/pipeline-response.js
@@ -81,18 +81,23 @@ const pipelineResponse = async (io, message) => {
   }
 
   const {
-    processingConfig: currentConfig,
+    processingConfig: previousConfig,
   } = await experimentService.getProcessingConfig(experimentId);
 
   if (sampleUuid !== '') {
-    const { defaultFilterSettings = null } = currentConfig[taskName][sampleUuid];
+    const { auto } = output.config;
+
+    // This is a temporary fix to save defaultFilterSettings calculated in the QC pipeline
+    // to patch for old experiments with hardcoded defaultFilterSettings.
+    // Remove this once we're done migrating to the new experiment schema with defaultFilterSettings
+    if (auto) output.config.defaultFilterSettings = output.config.filterSettings;
 
     await experimentService.updateProcessingConfig(experimentId, [
       {
         name: taskName,
         body: {
-          ...currentConfig[taskName],
-          [sampleUuid]: { defaultFilterSettings, ...output.config },
+          ...previousConfig[taskName],
+          [sampleUuid]: { ...output.config },
         },
       },
     ]);
