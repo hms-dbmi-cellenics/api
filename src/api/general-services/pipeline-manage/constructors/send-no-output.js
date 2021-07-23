930f9f34f5ead5c3f029aafc1dada7391f5c0ac8
stefanbabukov <stefanbabukov98@gmail.com>
2021-07-23 12:14:06 +0300

    some changes

--- src/api/general-services/pipeline-manage/constructors/send-no-output.js
+++ src/api/general-services/pipeline-manage/constructors/send-no-output.js
@@ -1,10 +1,10 @@
 const { createTask, buildParams } = require('./create-new-step');
 
 const sendNoOutput = (context, step, stepArgs) => {
-  const { activityArn } = context;
+  // const { activityArn } = context;
   const { taskName } = stepArgs;
   const task = createTask(taskName, context); // check if these are the right ones
-  const params = buildParams(task, context, stepArgs); // check if these are the right ones
+  // const params = buildParams(task, context, stepArgs); // check if these are the right ones
 
   // modify this return for the proper message
   // return {
