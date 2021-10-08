73da10647bb90ae5c7ba11f2befc7d70d9420f00
Pol Alvarez <pol.avms@gmail.com>
2021-10-08 17:22:26 +0200

    added missing files

---
+++ tests/api/utils/hooks/pipeline-assign-no-pods.test.js
@@ -0,0 +1,47 @@
+const k8s = require('@kubernetes/client-node');
+const constants = require('../../../../src/api/general-services/pipeline-manage/constants');
+const fake = require('../../../test-utils/constants');
+const { buildPodRequest } = require('../../../../src/api/general-services/pipeline-manage/constructors/assign-pod-to-pipeline');
+
+jest.mock('@kubernetes/client-node');
+
+const mockEmptyPodsList = {
+  body: {
+    items: [],
+  },
+};
+
+const removeNamespacedPod = jest.fn();
+const patchNamespacedPod = jest.fn();
+const listNamespacedPod = jest.fn(() => mockEmptyPodsList);
+const mockApi = {
+  removeNamespacedPod,
+  patchNamespacedPod,
+  listNamespacedPod,
+};
+
+k8s.KubeConfig.mockImplementation(() => {
+  console.debug('mocking the constructor');
+  return {
+    loadFromDefault: jest.fn(),
+    makeApiClient: (() => mockApi),
+  };
+});
+
+const pipelineAssign = require('../../../../src/utils/hooks/pipeline-assign');
+
+describe('tests for the pipeline-assign service', () => {
+  it('does not call delete because there are not assigned pods', async () => {
+    const message = buildPodRequest(fake.SANDBOX_ID,
+      fake.EXPERIMENT_ID,
+      constants.ASSIGN_POD_TO_PIPELINE,
+      constants.GEM2S_PROCESS_NAME,
+      fake.ACTIVITY_ID);
+
+    await pipelineAssign.assignPodToPipeline(message);
+
+    expect(listNamespacedPod).toHaveBeenCalledTimes(2);
+    expect(removeNamespacedPod).toHaveBeenCalledTimes(0);
+    expect(patchNamespacedPod).toHaveBeenCalledTimes(0);
+  });
+});
