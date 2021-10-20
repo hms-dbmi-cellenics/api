bbb69f2e5a61408eba0da49f330136a1f99e488c
Pol Alvarez <pol.avms@gmail.com>
2021-10-20 12:57:14 +0200

    added registerAll method to hookRunner

--- src/utils/hooks/hookRunner.js
+++ src/utils/hooks/hookRunner.js
@@ -2,12 +2,15 @@ const getLogger = require('../getLogger');
 
 const logger = getLogger();
 
+const ALL = 'all';
 class hookRunner {
   constructor() {
     this.hooks = {};
     this.results = {};
   }
 
+  // register will run callback function when the taskName matches the payload
+  // received by by run
   register(taskName, callback) {
     if (this.hooks[taskName] === undefined) this.hooks[taskName] = [];
 
@@ -16,23 +19,42 @@ class hookRunner {
     } else {
       this.hooks[taskName].push(callback);
     }
+  }
 
-    this.results[taskName] = [];
+  // registerAll will run the callback function for any payload received
+  // the results will be assigned to each specific task though.
+  registerAll(callback) {
+    if (this.hooks[ALL] === undefined) this.hooks[ALL] = [];
+
+    if (Array.isArray(callback)) {
+      this.hooks[ALL].push(...callback);
+    } else {
+      this.hooks[ALL].push(callback);
+    }
   }
 
   // run requires taskName to be present as a key in the payload
   async run(payload) {
     const { taskName } = payload;
-    if (this.hooks[taskName] === undefined
-      || this.hooks[taskName].length === 0
-    ) { return null; }
+    this.results[taskName] = [];
 
     // Manual looping is done to prevent passing function in hooks[taskName] into a callback,
     // which might cause scoping issues
-    for (let idx = 0; idx < this.hooks[taskName].length; idx += 1) {
+
+    // Runs task specific hooks
+    for (let i = 0; this.hooks[taskName] !== undefined && i < this.hooks[taskName].length; i += 1) {
       // eslint-disable-next-line no-await-in-loop
-      this.results[taskName].push(await this.hooks[taskName][idx](payload));
+      this.results[taskName].push(this.hooks[taskName][i](payload));
     }
+
+    // Runs hooks that apply to all tasks (assigning the results to current task)
+
+    for (let i = 0; this.hooks[ALL] !== undefined && i < this.hooks[ALL].length; i += 1) {
+      this.results[taskName].push(this.hooks[ALL][i](payload));
+    }
+
+    // Wait for the resolution of all hooks at the same time
+    this.results[taskName] = await Promise.all(this.results[taskName]);
     logger.log(`Completed ${this.results[taskName].length} hooks for pipeline task ${taskName}`);
 
     return this.results;
--- tests/utils/hookRunner.test.js
+++ tests/utils/hookRunner.test.js
@@ -63,4 +63,46 @@ describe('HookRunner', () => {
 
     expect(runner.results.event).toEqual([fn1(), fn2()]);
   });
+
+  it('should run multiple hooks with a global one', async () => {
+    const fn1 = jest.fn(() => 1);
+    const fn2 = jest.fn(() => 2);
+    const fn3 = jest.fn(() => 3);
+
+    const runner = new HookRunner();
+
+    runner.register('event1', fn1);
+    runner.register('event2', fn3);
+    runner.registerAll(fn2);
+
+    await runner.run({ taskName: 'event1' });
+    await runner.run({ taskName: 'event2' });
+    await runner.run({ taskName: 'event3' });
+
+    expect(fn1).toHaveBeenCalledTimes(1);
+    expect(fn2).toHaveBeenCalledTimes(3);
+    expect(fn3).toHaveBeenCalledTimes(1);
+
+    expect(runner.results.event1).toEqual([fn1(), fn2()]);
+    expect(runner.results.event2).toEqual([fn3(), fn2()]);
+    expect(runner.results.event3).toEqual([fn2()]);
+  });
+
+
+  it('should run multiple times a global hook', async () => {
+    const fn1 = jest.fn(() => 1);
+
+    const runner = new HookRunner();
+
+    runner.registerAll([fn1]);
+
+    await runner.run({ taskName: 'event-1' });
+    await runner.run({ taskName: 'event-2' });
+    await runner.run({ taskName: 'event-3' });
+
+    expect(fn1).toHaveBeenCalledTimes(3);
+    expect(runner.results['event-1']).toEqual([fn1()]);
+    expect(runner.results['event-2']).toEqual([fn1()]);
+    expect(runner.results['event-3']).toEqual([fn1()]);
+  });
 });
