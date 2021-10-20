2d81e78637874936f9e2ae6d1b3e95ec08b6cfaf
Pol Alvarez <pol.avms@gmail.com>
2021-10-20 16:56:21 +0200

    removing state machines logic checking if there are pods assigned

--- src/api/general-services/pipeline-manage/constructors/assign-pod-to-pipeline.js
+++ src/api/general-services/pipeline-manage/constructors/assign-pod-to-pipeline.js
@@ -23,78 +23,36 @@ const buildPodRequest = (sandboxId, experimentId, taskName, processName, activit
   },
 });
 
-const getRunningPods = (context, step) => {
-  const { clusterInfo, experimentId } = context;
+// const getRunningPods = (context, step) => {
+//   const { clusterInfo, experimentId } = context;
 
-  return {
-    ...step,
-    Type: 'Task',
-    Comment: 'Retrieves running pods assigned to the experiment ID.',
-    Resource: 'arn:aws:states:::eks:call',
-    Parameters: {
-      ClusterName: clusterInfo.name,
-      CertificateAuthority: clusterInfo.certAuthority,
-      Endpoint: clusterInfo.endpoint,
-      Method: 'GET',
-      Path: `/api/v1/namespaces/${config.pipelineNamespace}/pods`,
-      QueryParameters: {
-        labelSelector: [
-          `experimentId=${experimentId},type=pipeline`,
-        ],
-        fieldSelector: [
-          'status.phase=Running',
-        ],
-      },
-    },
-  };
-};
+//   return {
+//     ...step,
+//     Type: 'Task',
+//     Comment: 'Retrieves running pods assigned to the experiment ID.',
+//     Resource: 'arn:aws:states:::eks:call',
+//     Parameters: {
+//       ClusterName: clusterInfo.name,
+//       CertificateAuthority: clusterInfo.certAuthority,
+//       Endpoint: clusterInfo.endpoint,
+//       Method: 'GET',
+//       Path: `/api/v1/namespaces/${config.pipelineNamespace}/pods`,
+//       QueryParameters: {
+//         labelSelector: [
+//           `experimentId=${experimentId},type=pipeline`,
+//         ],
+//         fieldSelector: [
+//           'status.phase=Running',
+//         ],
+//       },
+//     },
+//   };
+// };
 
-const deleteRunningPods = (context, step) => {
-  const { clusterInfo } = context;
-
-  return {
-    ...step,
-    Type: 'Map',
-    ItemsPath: '$.runningPods.ResponseBody.items',
-    MaxConcurrency: 0,
-    Iterator: {
-      StartAt: 'DeletePod',
-      States: {
-        DeletePod: {
-          Type: 'Task',
-          End: true,
-          Comment: 'Patch pod to set the activity ID label.',
-          Resource: 'arn:aws:states:::eks:call',
-          Parameters: {
-            ClusterName: clusterInfo.name,
-            CertificateAuthority: clusterInfo.certAuthority,
-            Endpoint: clusterInfo.endpoint,
-            Method: 'DELETE',
-            'Path.$': "States.Format('/api/v1/namespaces/{}/pods/{}', $.metadata.namespace, $.metadata.name)",
-          },
-          Catch: [
-            {
-              ErrorEquals: [
-                'EKS.404',
-              ],
-              Next: 'Ignore404',
-            },
-          ],
-        },
-        Ignore404: {
-          Type: 'Pass',
-          End: true,
-        },
-      },
-    },
-  };
-};
-
-const assignPodToPipeline = (context, step) => {
+const requestPod = (context, step) => {
   const {
-    clusterInfo, environment, accountId, sandboxId, activityArn, experimentId, processName,
+    environment, accountId, sandboxId, activityArn, experimentId, processName,
   } = context;
-
   const activityId = getActivityId(activityArn);
 
   const requestPodMessage = buildPodRequest(sandboxId,
@@ -106,57 +64,11 @@ const assignPodToPipeline = (context, step) => {
   return {
     ...step,
     Type: 'Map',
-    ItemsPath: '$.retries',
-    MaxConcurrency: 1,
-    // retry waits up to 226 seconds, fargate takes from 1 to 3 minutes to spawn a new pod
-    // total wait time = IntervalSeconds[(1 - BackoffRate^(MaxAttempts))/(1-BackoffRate)]
-    Retry: [{
-      ErrorEquals: ['NoPodsAvailable'],
-      IntervalSeconds: 2,
-      MaxAttempts: 10,
-      BackoffRate: 1.5,
-    }],
+    ItemsPath: '$.runningPods.ResponseBody.items',
+    MaxConcurrency: 0,
     Iterator: {
-      StartAt: 'GetUnassignedPod',
+      StartAt: 'DeletePod',
       States: {
-        GetUnassignedPod: {
-          Next: 'IsPodAvailable',
-          Type: 'Task',
-          Comment: 'Retrieves first unassigned and running pipeline pod.',
-          Resource: 'arn:aws:states:::eks:call',
-          Parameters: {
-            ClusterName: clusterInfo.name,
-            CertificateAuthority: clusterInfo.certAuthority,
-            Endpoint: clusterInfo.endpoint,
-            Method: 'GET',
-            Path: `/api/v1/namespaces/${config.pipelineNamespace}/pods`,
-            QueryParameters: {
-              labelSelector: [
-                '!activityId,type=pipeline',
-              ],
-              fieldSelector: [
-                'status.phase=Running',
-              ],
-            },
-          },
-        },
-        IsPodAvailable: {
-          Type: 'Choice',
-          Comment: 'Redirects to an error state if there are no available pods.',
-          Choices: [
-            {
-              Variable: '$.ResponseBody.items[0]',
-              IsPresent: false,
-              Next: 'NoPodsAvailable',
-            },
-          ],
-          Default: 'RequestPod',
-        },
-        NoPodsAvailable: {
-          Type: 'Fail',
-          Cause: 'No available and running pipeline pods.',
-          Error: 'NoPodsAvailable',
-        },
         RequestPod: {
           Comment: 'Send a message through SNS so that the API assigns a pod to the pipeline',
           Type: 'Task',
@@ -178,6 +90,48 @@ const assignPodToPipeline = (context, step) => {
   };
 };
 
+// const assignPodToPipeline = (context, step) => {
+//   const activityId = getActivityId(activityArn);
+
+
+//   return {
+//     ...step,
+//     Type: 'Map',
+//     ItemsPath: '$.retries',
+//     MaxConcurrency: 1,
+//     // retry waits up to 226 seconds, fargate takes from 1 to 3 minutes to spawn a new pod
+//     // total wait time = IntervalSeconds[(1 - BackoffRate^(MaxAttempts))/(1-BackoffRate)]
+//     Retry: [{
+//       ErrorEquals: ['NoPodsAvailable'],
+//       IntervalSeconds: 2,
+//       MaxAttempts: 10,
+//       BackoffRate: 1.5,
+//     }],
+//     Iterator: {
+//       StartAt: 'RequestPod',
+//       States: {
+//         RequestPod: {
+//           Comment: 'Send a message through SNS so that the API assigns a pod to the pipeline',
+//           Type: 'Task',
+//           Resource: 'arn:aws:states:::sns:publish',
+//           End: true,
+//           Parameters: {
+//             TopicArn:
+// `arn:aws:sns:${config.awsRegion}:${accountId}:work-results-${environment}-${sandboxId}`,
+//             Message: JSON.stringify(requestPodMessage),
+//             MessageAttributes: {
+//               type: {
+//                 DataType: 'String',
+//                 StringValue: 'PipelineResponse',
+//               },
+//             },
+//           },
+//         },
+//       },
+//     },
+//   };
+// };
+
 
 const waitForPod = (context, step) => {
   const { clusterInfo, activityArn } = context;
@@ -194,7 +148,7 @@ const waitForPod = (context, step) => {
     Retry: [{
       ErrorEquals: ['NoPodAssigned'],
       IntervalSeconds: 1,
-      MaxAttempts: 9,
+      MaxAttempts: 12,
       BackoffRate: 1.5,
     }],
     Catch: [{
@@ -250,5 +204,5 @@ const waitForPod = (context, step) => {
 };
 
 module.exports = {
-  buildPodRequest, getRunningPods, deleteRunningPods, assignPodToPipeline, waitForPod,
+  buildPodRequest, requestPod, waitForPod,
 };
