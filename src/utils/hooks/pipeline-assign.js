b53c4b57188c171ff0fb76c4941653abef28de84
Pol Alvarez <pol.avms@gmail.com>
2021-10-22 10:55:19 +0200

    refactored pipeline-assign using sleep

--- src/utils/hooks/pipeline-assign.js
+++ src/utils/hooks/pipeline-assign.js
@@ -1,5 +1,6 @@
 /* eslint-disable no-await-in-loop */
 const k8s = require('@kubernetes/client-node');
+const sleep = require('util').promisify(setTimeout);
 const getLogger = require('../getLogger');
 const validateRequest = require('../schema-validator');
 const constants = require('../../api/general-services/pipeline-manage/constants');
@@ -10,18 +11,9 @@ const kc = new k8s.KubeConfig();
 kc.loadFromDefault();
 
 
-const getPods = async (k8sApi, namespace, activityId) => {
-  const [assignedPods, unassignedPods] = await Promise.all(
-    [
-      k8sApi.listNamespacedPod(namespace, null, null, null, 'status.phase=Running', `activityId=${activityId},type=pipeline`),
-      k8sApi.listNamespacedPod(namespace, null, null, null, 'status.phase=Running', '!activityId,type=pipeline'),
-    ],
-  );
+const removeRunningPods = async (k8sApi, namespace, activityId) => {
+  const assignedPods = await k8sApi.listNamespacedPod(namespace, null, null, null, 'status.phase=Running', `activityId=${activityId},type=pipeline`);
 
-  return [assignedPods, unassignedPods];
-};
-
-const removeRunningPods = async (k8sApi, namespace, assignedPods) => {
   await Promise.all(assignedPods.body.items.map((pod) => {
     const { name } = pod.metadata;
     logger.log(`Found pipeline running pod ${name}, removing...`);
@@ -36,6 +28,11 @@ const patchPod = async (k8sApi,
   activityId,
   processName) => {
   const pods = unassignedPods.body.items;
+
+  if (pods.length < 1) {
+    throw new Error('no unassigned pods available');
+  }
+
   logger.log(pods.length, 'unassigned candidate pods found. Selecting one...');
 
   // Select a pod to run this experiment on.
@@ -65,6 +62,31 @@ const patchPod = async (k8sApi,
     });
 };
 
+const patchPodWithRetries = async (k8sApi, maxAttempts, backoffRate, message) => {
+  const { experimentId, input: { sandboxId, activityId, processName } } = message;
+  const namespace = `pipeline-${sandboxId}`;
+
+  for (let retry = 0; retry < maxAttempts; retry += 1) {
+    try {
+      const unassignedPods = await k8sApi.listNamespacedPod(namespace, null, null, null, 'status.phase=Running', '!activityId,type=pipeline');
+      await patchPod(k8sApi, namespace, unassignedPods, experimentId, activityId, processName);
+      return;
+    } catch (e) {
+      // retry waits up to 226 seconds, fargate takes from 1 to 3 minutes to spawn a new pod
+      // total wait time = IntervalSeconds*[(1 - BackoffRate^(MaxAttempts))/(1-BackoffRate)]
+      // using IntervalSeconds = 1
+
+      if (retry < maxAttempts) {
+        const timeout = (backoffRate ** (retry + 1)) * 1000; // in ms
+        logger.log(`Failed to patch pod: ${e}. Retry [${retry}]. Waiting for ${timeout} ms before trying again. `);
+        await sleep(timeout);
+      } else {
+        logger.log(`Failed to patch pod after ${maxAttempts} retries.`);
+      }
+    }
+  }
+};
+
 const assignPodToPipeline = async (message) => {
   // this checks should be refactored and cleaned once the gem2s / qc spec refactors are done
   // and we can be sure that taskName is always present at the top-level of all the message
@@ -77,33 +99,17 @@ const assignPodToPipeline = async (message) => {
   await validateRequest(message, 'PipelinePodRequest.v1.yaml');
 
   const k8sApi = kc.makeApiClient(k8s.CoreV1Api);
-
   const { experimentId, input: { sandboxId, activityId, processName } } = message;
-  const namespace = `pipeline-${sandboxId}`;
 
+  logger.log(`Trying to assign pod to ${processName} pipeline for experiment ${experimentId} in sandbox ${sandboxId} for activity ${activityId}`);
+
+  // remove pipeline pods already assigned to this experiment
+  await removeRunningPods(k8sApi, message);
+
+  // try to choose a free pod and assign it to the current pipeline
   const maxAttempts = 12;
   const backoffRate = 1.5;
-  let isPodAssigned = false;
-
-  logger.log(`Assigning pod to ${processName} pipeline for experiment ${experimentId} in sandbox ${sandboxId} for activity ${activityId}`);
-  // try to choose a free pod and assign it to the current pipeline
-  for (let i = 0; !isPodAssigned && i < maxAttempts; i += 1) {
-    const [assignedPods, unassignedPods] = await getPods(k8sApi, namespace, activityId);
-    await removeRunningPods(k8sApi, namespace, assignedPods);
-    try {
-      await patchPod(k8sApi, namespace, unassignedPods, experimentId, activityId, processName);
-      isPodAssigned = true;
-    } catch (e) {
-      // retry waits up to 226 seconds, fargate takes from 1 to 3 minutes to spawn a new pod
-      // total wait time = IntervalSeconds*[(1 - BackoffRate^(MaxAttempts))/(1-BackoffRate)]
-      // using IntervalSeconds = 1
-      const timeout = (backoffRate ** (i + 1)) * 1000; // in ms
-      logger.log(`[${i}] Error patching pod: ${e}. Waiting for ${timeout} seconds before trying again. `);
-      await new Promise((resolve) => setTimeout(() => {
-        resolve();
-      }, timeout));
-    }
-  }
+  await patchPodWithRetries(k8sApi, maxAttempts, backoffRate, message);
 };
 
 module.exports = { assignPodToPipeline };
--- tests/api/utils/hooks/pipeline-assign-no-pods.test.js
+++ tests/api/utils/hooks/pipeline-assign-no-pods.test.js
@@ -1,6 +1,14 @@
+
+const util = require('util');
+
+jest.mock('util');
+const setTimeout = jest.fn();
+util.promisify.mockImplementation(() => setTimeout);
+
 const k8s = require('@kubernetes/client-node');
 const constants = require('../../../../src/api/general-services/pipeline-manage/constants');
 const fake = require('../../../test-utils/constants');
+
 const { buildPodRequest } = require('../../../../src/api/general-services/pipeline-manage/constructors/assign-pod-to-pipeline');
 
 jest.mock('@kubernetes/client-node');
@@ -29,6 +37,7 @@ k8s.KubeConfig.mockImplementation(() => {
 });
 
 const pipelineAssign = require('../../../../src/utils/hooks/pipeline-assign');
+// const util = require('../../../../__mocks__/utils');
 
 describe('tests for the pipeline-assign service', () => {
   it('does not call delete because there are not assigned pods', async () => {
@@ -38,10 +47,15 @@ describe('tests for the pipeline-assign service', () => {
       constants.GEM2S_PROCESS_NAME,
       fake.ACTIVITY_ID);
 
+    // jest.useFakeTimers();
+
     await pipelineAssign.assignPodToPipeline(message);
 
-    expect(listNamespacedPod).toHaveBeenCalledTimes(24);
+    expect(listNamespacedPod).toHaveBeenCalledTimes(13);
     expect(deleteNamespacedPod).toHaveBeenCalledTimes(0);
     expect(patchNamespacedPod).toHaveBeenCalledTimes(0);
+
+    expect(setTimeout).toHaveBeenCalledTimes(12);
+    expect(setTimeout).toHaveBeenLastCalledWith(129746.337890625);
   });
 });
