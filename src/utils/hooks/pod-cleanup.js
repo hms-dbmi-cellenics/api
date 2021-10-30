b791483408818e15ffc56329d3ce3eff2605ab0c
Pol Alvarez <pol.avms@gmail.com>
2021-10-30 13:10:44 +0200

    using namespace from config

--- src/utils/hooks/pod-cleanup.js
+++ src/utils/hooks/pod-cleanup.js
@@ -1,6 +1,7 @@
 /* eslint-disable no-await-in-loop */
 const k8s = require('@kubernetes/client-node');
 const getLogger = require('../getLogger');
+const config = require('../../config');
 
 const logger = getLogger();
 
@@ -9,8 +10,8 @@ kc.loadFromDefault();
 
 
 const removeRunningPods = async (k8sApi, message) => {
-  const { experimentId, input: { sandboxId } } = message;
-  const namespace = `pipeline-${sandboxId}`;
+  const { experimentId } = message;
+  const namespace = `pipeline-${config.sandboxId}`;
   const assignedPods = await k8sApi.listNamespacedPod(namespace, null, null, null, null, `experimentId=${experimentId},type=pipeline`);
 
   await Promise.all(assignedPods.body.items.map((pod) => {
@@ -23,9 +24,9 @@ const removeRunningPods = async (k8sApi, message) => {
 
 const cleanupPods = async (message) => {
   const k8sApi = kc.makeApiClient(k8s.CoreV1Api);
-  const { experimentId, input: { sandboxId, activityId } } = message;
+  const { experimentId } = message;
 
-  logger.log(`Removing pipeline pods for experiment ${experimentId} in sandbox ${sandboxId} for activity ${activityId}`);
+  logger.log(`Removing pipeline pods for experiment ${experimentId} in sandbox ${config.sandboxId}`);
   await removeRunningPods(k8sApi, message);
 };
 
