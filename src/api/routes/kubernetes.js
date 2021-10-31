6537faa7e800e7e01e2a619c5d957bd7b35c7a20
Pol Alvarez <pol.avms@gmail.com>
2021-10-31 13:55:33 +0100

    refactored kubernetes handling a bit

--- src/api/routes/kubernetes.js
+++ src/api/routes/kubernetes.js
@@ -6,47 +6,28 @@ const config = require('../../config');
 const kc = new k8s.KubeConfig();
 kc.loadFromDefault();
 
-// const { expressAuthorizationMiddleware } = require('../../utils/authMiddlewares');
 
 const logger = getLogger();
 
 module.exports = {
-  // 'kubernetes#event': [
-  // expressAuthorizationMiddleware,
   'kubernetes#event': async (req, res) => {
     logger.log('received kubernetes event');
 
-    try {
-      const { reason, message, involvedObject: { name, namespace } } = req.body;
-      logger.log(`[${reason}]received kubernetes event: ${message} ${name} in ${namespace}`);
+    const { reason, message, involvedObject: { name, namespace } } = req.body;
+    logger.log(`[${reason}] received kubernetes event: ${message} ${name} in ${namespace}`);
 
-      if (namespace !== `pipeline-${config.sandboxId}`) {
-        logger.log(`ignoring event from namespace ${namespace}.`);
-        res.status(200).send('ok');
-
-        return;
+    // remove only pods in your namespace and due to backoff errors
+    if (namespace === `pipeline-${config.sandboxId}` && reason === 'BackOff' && config.clusterEnv !== 'development') {
+      try {
+        const k8sApi = kc.makeApiClient(k8s.CoreV1Api);
+        logger.log(`removing pod ${name} in ${namespace}`);
+        await k8sApi.deleteNamespacedPod(name, namespace);
+      } catch (e) {
+        logger.error('error processing k8s event', e);
+        AWSXRay.getSegment().addError(e);
       }
-
-      if (reason !== 'BackOff' || config.clusterEnv === 'development') {
-        logger.log(`ignoring event ${reason} in  ${config.clusterEnv} env.`);
-        res.status(200).send('ok');
-        return;
-      }
-
-      const k8sApi = kc.makeApiClient(k8s.CoreV1Api);
-      logger.log(`removing pod ${name} in ${namespace}`);
-      await k8sApi.deleteNamespacedPod(name, namespace);
-    } catch (e) {
-      logger.error('error processing k8s event');
-      // logger.log(req);
-      logger.error(e);
-      AWSXRay.getSegment().addError(e);
-      res.status(200).send('nok');
-      return;
     }
-    logger.log(req);
 
     res.status(200).send('ok');
   },
-  // ],
 };
