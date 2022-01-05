4bed0a6e514e82b8bc3e4be95819258cea0285f2
Pol Alvarez <pol.avms@gmail.com>
2022-01-05 12:45:53 +0100

    wrapper kubernetes endpoint in try catch

--- src/api/routes/kubernetes.js
+++ src/api/routes/kubernetes.js
@@ -9,24 +9,23 @@ kc.loadFromDefault();
 const logger = getLogger();
 
 module.exports = {
-  'kubernetes#event': async (req, res) => {
+  'kubernetes#event': async (req, res, next) => {
     logger.log('received kubernetes event');
+    try {
+      const { reason, message, involvedObject: { name, namespace } } = req.body;
+      logger.log(`[${reason}] received kubernetes event: ${message} ${name} in ${namespace}`);
 
-    const { reason, message, involvedObject: { name, namespace } } = req.body;
-    logger.log(`[${reason}] received kubernetes event: ${message} ${name} in ${namespace}`);
-
-    // remove only pods in your namespace and due to backoff errors
-    if ((namespace.match('^pipeline-.*') || namespace.match('^worker-.*')) && reason === 'BackOff') {
-      try {
+      // remove only pods in your namespace and due to backoff errors
+      if ((namespace.match('^pipeline-.*') || namespace.match('^worker-.*')) && reason === 'BackOff') {
         const k8sApi = kc.makeApiClient(k8s.CoreV1Api);
         logger.log(`removing pod ${name} in ${namespace}`);
         await k8sApi.deleteNamespacedPod(name, namespace);
-      } catch (e) {
-        logger.error('error processing k8s event', e);
-        AWSXRay.getSegment().addError(e);
       }
+      res.status(200).send('ok');
+    } catch (e) {
+      logger.error('error processing k8s event', e);
+      AWSXRay.getSegment().addError(e);
+      next(e);
     }
-
-    res.status(200).send('ok');
   },
 };
