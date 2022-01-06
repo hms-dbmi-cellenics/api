8bf37aefd6c612ffffa5a7e3de1bf2612369de3a
Pol Alvarez <pol.avms@gmail.com>
2022-01-06 12:00:37 +0100

    removed biomage-ltd from configs

--- src/config/test-config.js
+++ src/config/test-config.js
@@ -16,10 +16,7 @@ module.exports = {
   workerNamespace: 'worker-test-namespace',
   pipelineNamespace: 'pipeline-test-namespace',
   workerInstanceConfigUrl: 'https://raw.githubusercontent.com/hms-dbmi-cellenics/iac/master/releases/production/worker.yaml',
-  // TODO: remove this line when scaling work is merged into production and thus the
-  // production/pipeline.yaml is updated to new name pipelineRunner
   pipelineInstanceConfigUrl: 'https://raw.githubusercontent.com/hms-dbmi-cellenics/iac/master/releases/staging/pipeline.yaml',
-  // pipelineInstanceConfigUrl: 'https://raw.githubusercontent.com/hms-dbmi-cellenics/iac/master/releases/production/pipeline.yaml',
   api: {
     prefix: '/',
   },
