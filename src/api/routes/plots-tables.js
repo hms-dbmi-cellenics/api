670387a1c78e062ca44a0aab1102d3ea0a4bb41e
stefanbabukov <stefanbabukov98@gmail.com>
2022-01-06 21:09:56 +0200

    removed unused endpoints

--- src/api/routes/plots-tables.js
+++ src/api/routes/plots-tables.js
@@ -5,13 +5,6 @@ const { NotFoundError } = require('../../utils/responses');
 const plotsTablesService = new PlotsTablesService();
 
 module.exports = {
-  'plots-tables#create': (req, res, next) => {
-    const { experimentId, plotUuid } = req.params;
-
-    plotsTablesService.create(experimentId, plotUuid, req.body)
-      .then((response) => res.json(response))
-      .catch(next);
-  },
   'plots-tables#read': (req, res, next) => {
     const { experimentId, plotUuid } = req.params;
     plotsTablesService.read(experimentId, plotUuid)
@@ -29,13 +22,6 @@ module.exports = {
     const { experimentId, plotUuid } = req.params;
 
     plotsTablesService.create(experimentId, plotUuid, req.body)
-      .then((response) => res.json(response))
-      .catch(next);
-  },
-  'plots-tables#delete': (req, res, next) => {
-    const { experimentId, plotUuid } = req.params;
-
-    plotsTablesService.delete(experimentId, plotUuid)
       .then((response) => res.json(response))
       .catch(next);
   },
