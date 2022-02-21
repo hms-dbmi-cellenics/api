df469a9d283b24230e38d23cf3cd71e410603230
Anugerah Erlaut <aerlaut@live.com>
2022-02-21 18:25:14 +0000

    update samples patch record instea of overwriting

--- src/api/route-services/samples.js
+++ src/api/route-services/samples.js
@@ -81,27 +81,39 @@ class SamplesService {
   async updateSamples(projectUuid, experimentId, body) {
     logger.log(`Updating samples for project ${projectUuid} and expId ${experimentId}`);
 
+    const dynamodb = createDynamoDbInstance();
+
     const marshalledKey = convertToDynamoDbRecord({
       experimentId,
     });
 
+    const getSamplesParams = {
+      TableName: this.samplesTableName,
+      Key: marshalledKey,
+      ProjectionExpression: 'samples',
+    };
+
+    // Merge samples record with the body
+    const response = await dynamodb.getItem(getSamplesParams).promise();
+    const { samples } = convertToJsObject(response.Item);
+
+    const updatedSamples = _.merge(samples, body);
+
     const marshalledData = convertToDynamoDbRecord({
-      ':samples': body,
+      ':samples': updatedSamples,
       ':projectUuid': projectUuid,
     });
 
     // Update samples
-    const params = {
+    const updateSamplesParams = {
       TableName: this.samplesTableName,
       Key: marshalledKey,
       UpdateExpression: 'SET samples = :samples, projectUuid = :projectUuid',
       ExpressionAttributeValues: marshalledData,
     };
 
-    const dynamodb = createDynamoDbInstance();
-
     try {
-      await dynamodb.updateItem(params).send();
+      await dynamodb.updateItem(updateSamplesParams).send();
       return OK();
     } catch (e) {
       if (e.statusCode === 404) throw new NotFoundError('Project not found');
