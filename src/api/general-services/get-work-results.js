2825e6379a1f920b3608a9eaa70e94ba315f3970
ivababukova <iva@biomage.net>
2022-02-22 11:50:04 +0100

    move the s3 tags check to a separate func

--- src/api/general-services/get-work-results.js
+++ src/api/general-services/get-work-results.js
@@ -7,25 +7,21 @@ const { getSignedUrl } = require('../../utils/aws/s3');
 
 const logger = getLogger();
 
-const getWorkResults = async (experimentId, ETag) => {
-  const s3 = new AWS.S3();
-  const params = {
-    Bucket: `worker-results-${config.clusterEnv}`,
-    Key: ETag,
-  };
-
+/**
+ * Form of authorization based on S3 tags
+ * Validates that the tag matches the authorized experiment id
+ * If they don't match, throws an exception
+ */
+const validateTagMatching = async (experimentId, params) => {
   let objectTagging = [];
+  const s3 = new AWS.S3();
 
-  // once we get here, the authorization middleware has already run so we
-  // know that the user has permissions to access the given experiment_id
-  // however, we need to verify that the given ETag matches the authorized
-  // experimentId
   try {
     objectTagging = await s3.getObjectTagging(params).promise();
   } catch (err) {
     // the API asks for the results before they are available, we just return a not found
     if (err.code === 'NoSuchKey') {
-      throw new NotFoundError(`Couldn't find s3 worker results bucket with key: ${ETag}`);
+      throw new NotFoundError(`Couldn't find s3 worker results bucket with key: ${params.Key}`);
     }
 
     logger.log('Error received while getting object tags', err);
@@ -36,7 +32,15 @@ const getWorkResults = async (experimentId, ETag) => {
   if (experimentIdTag !== experimentId) {
     throw new UnauthorizedError('User is not authorized to get worker results for this experiment');
   }
+};
 
+const getWorkResults = async (experimentId, ETag) => {
+  const params = {
+    Bucket: `worker-results-${config.clusterEnv}`,
+    Key: ETag,
+  };
+
+  await validateTagMatching(experimentId, params);
   const signedUrl = getSignedUrl('getObject', params);
   return { signedUrl };
 };
