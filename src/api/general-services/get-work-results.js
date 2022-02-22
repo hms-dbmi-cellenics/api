f930241d6604b858eebef4fa05dd44f7134cf9e0
Pol Alvarez <pol.avms@gmail.com>
2022-02-22 09:15:44 +0100

    added explicit not found handling to worker results

--- src/api/general-services/get-work-results.js
+++ src/api/general-services/get-work-results.js
@@ -16,17 +16,26 @@ const getWorkResults = async (experimentId, ETag) => {
 
   let objectTagging = [];
 
+  // once we get here, the authorization middleware has already run so we
+  // know that the user has permissions to access the given experiment_id
+  // however, we need to verify that the given ETag matches the authorized
+  // experimentId
   try {
     objectTagging = await s3.getObjectTagging(params).promise();
   } catch (err) {
+    // the API asks for the results before they are available, we just return a not found
+    if (err.code === 'NoSuchKey') {
+      throw new NotFoundError(`Couldn't find s3 worker results bucket with key: ${ETag}`);
+    }
+
     logger.log('Error received while getting object tags', err);
+    throw err;
   }
-  if (!objectTagging.TagSet) {
-    throw new NotFoundError('Worker results not found');
-  }
+
   const experimentIdTag = objectTagging.TagSet.filter((tag) => tag.Key === 'experimentId')[0].Value;
   if (experimentIdTag !== experimentId) {
-    throw new UnauthorizedError('User is not authorized to get worker results for this experiment');
+    throw new UnauthorizedError(`User was authorized for experiment ${experimentId} but the requested
+    worker results belong to experiment ${experimentIdTag}.`);
   }
 
   const signedUrl = getSignedUrl('getObject', params);
