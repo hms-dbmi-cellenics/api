51c9523ee592aba9953d9cbae199219cad9e0fd9
stefanbabukov <stefanbabukov98@gmail.com>
2022-02-28 14:12:56 +0000

    update email template

--- src/utils/emailTemplates/buildUserInvitedEmailBody.js
+++ src/utils/emailTemplates/buildUserInvitedEmailBody.js
@@ -8,12 +8,12 @@ const buildUserInvitedEmailBody = (email, experimentId, inviterUser) => {
         <body>
             <p>
               Hello, <br/>
-              Your collaborator ${inviterUser.email} has invited you to explore their single cell RNA-seq project in Cellenics. <br/>
+              Your collaborator ${inviterUser.email} has invited you to explore their single cell RNA-seq project in Cellenics. <br/><br/>
               Access it with the link below: <br/>
               <a href="${link}">${link}</a> <br/> <br/>
               Cellenics is a user-friendly online tool for single cell RNA-seq data analysis. <br/>
               The platform is designed specifically for biologists, and it's completely free for academic researchers.<br/><br/>
-              More information about Cellenics can be found at <a href="https://biomage.net">biomage.net</a>.<br/>
+              More information about Cellenics can be found at <a href="https://biomage.net">biomage.net</a>.<br/><br/>
               If you need help or have any questions, please contact us at hello@biomage.net. <br/><br/>
               Best Regards, <br/>
               Cellenics team
