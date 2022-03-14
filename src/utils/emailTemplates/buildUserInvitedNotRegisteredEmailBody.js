ebe93dfcb130af11b18f0c132f8f4ae09e080f0a
stefanbabukov <stefanbabukov98@gmail.com>
2022-03-14 17:36:36 +0000

    add unregistered users to invite-access

---
+++ src/utils/emailTemplates/buildUserInvitedNotRegisteredEmailBody.js
@@ -0,0 +1,48 @@
+const buildUserInvitedNotRegisteredEmailBody = (email, inviterUser) => {
+  const messageToSend = `
+        <html>
+        <head>
+        </head>
+        <body>
+            <p>
+              Hello, <br/>
+              Your collaborator ${inviterUser.email} has invited you to explore their single cell RNA-seq project in Cellenics. <br/><br/>
+              You need to register to Cellenics to access it. <br/>
+              Register at <a href="https://scp.biomage.net">scp.biomage.net</a> <br/> <br/>
+
+              Cellenics is a user-friendly online tool for single cell RNA-seq data analysis. <br/>
+              The platform is designed specifically for biologists, and it's completely free for academic researchers.<br/><br/>
+              More information about Cellenics can be found at <a href="https://biomage.net">biomage.net</a>.<br/><br/>
+              If you need help or have any questions, please contact us at hello@biomage.net. <br/><br/>
+              Best Regards, <br/>
+              Cellenics team
+            <p/>
+        </body>
+      </html>`;
+
+
+  const params = {
+    Destination: {
+      ToAddresses: [email],
+    },
+    Message: {
+      Body: {
+        Text: {
+          Charset: 'UTF-8',
+          Data: messageToSend,
+        },
+        Html: {
+          Charset: 'UTF-8',
+          Data: messageToSend,
+        },
+      },
+      Subject: {
+        Charset: 'UTF-8',
+        Data: 'Invitation to join a project in Cellenics',
+      },
+    },
+    Source: 'notification@biomage.net',
+  };
+  return params;
+};
+module.exports = buildUserInvitedNotRegisteredEmailBody;
