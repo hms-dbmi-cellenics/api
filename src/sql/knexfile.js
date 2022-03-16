962e1b1209664f07ce374bc549c7dbacce87a98e
cosa65 <martin@biomage.net>
2022-03-16 17:11:30 -0300

    Select user according to whether it is a migraiton or the api

--- src/sql/knexfile.js
+++ src/sql/knexfile.js
@@ -2,8 +2,8 @@ const config = require('../config');
 const getConnectionParams = require('./getConnectionParams');
 
 // This is one of the shapes the knexfile can take https://knexjs.org/#knexfile
-const fetchConfiguration = async (environment) => {
-  const params = await getConnectionParams(environment);
+const fetchConfiguration = async (environment, username) => {
+  const params = await getConnectionParams(environment, username);
   return {
     [environment]: {
       client: 'postgresql',
@@ -13,9 +13,11 @@ const fetchConfiguration = async (environment) => {
 };
 
 module.exports = async () => {
+  // Check if this is being run for a migration or for the api
   const environment = process.env.MIGRATIONS_ENV || config.clusterEnv;
+  const username = process.env.MIGRATIONS_ENV ? 'dev_role' : 'api_role';
 
-  const configuration = await fetchConfiguration(environment);
+  const configuration = await fetchConfiguration(environment, username);
   return {
     ...configuration,
   };
