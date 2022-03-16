d49123aa7194ffe6f54de5dc0a8ac45b6c5c703c
cosa65 <martin@biomage.net>
2022-03-16 17:33:50 -0300

    Add some migration checks so that the api doesn't break

--- src/sql/knexfile.js
+++ src/sql/knexfile.js
@@ -1,9 +1,11 @@
+const _ = require('lodash');
+
 const config = require('../config');
 const getConnectionParams = require('./getConnectionParams');
 
 // This is one of the shapes the knexfile can take https://knexjs.org/#knexfile
-const fetchConfiguration = async (environment, username) => {
-  const params = await getConnectionParams(environment, username);
+const fetchConfiguration = async (environment, isAMigration) => {
+  const params = await getConnectionParams(environment, isAMigration);
   return {
     [environment]: {
       client: 'postgresql',
@@ -14,10 +16,10 @@ const fetchConfiguration = async (environment, username) => {
 
 module.exports = async () => {
   // Check if this is being run for a migration or for the api
+  const isAMigration = !_.isNil(process.env.MIGRATIONS_ENV);
   const environment = process.env.MIGRATIONS_ENV || config.clusterEnv;
-  const username = process.env.MIGRATIONS_ENV ? 'dev_role' : 'api_role';
 
-  const configuration = await fetchConfiguration(environment, username);
+  const configuration = await fetchConfiguration(environment, isAMigration);
   return {
     ...configuration,
   };
