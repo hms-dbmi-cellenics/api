29f36b125e71f1031e1ff44674cb91f8066105cb
cosa65 <martin@biomage.net>
2022-03-21 15:40:25 -0300

    Minor cleanup

--- src/sql/sqlClient.js
+++ src/sql/sqlClient.js
@@ -3,15 +3,19 @@ const { types } = require('pg');
 
 const TIMESTAMPZ = 1184;
 
+const setCustomParsers = () => {
+  // Default timestampz parser returns an object which breaks swagger's validation
+  // With this we can override that parser to return a string
+  types.setTypeParser(TIMESTAMPZ, (dateString) => dateString);
+};
+
 const sqlClient = (() => {
   let instance;
 
   return {
     get: (knexConfig) => {
       if (!instance && knexConfig) {
-        // Default timestampz parser returns an object which breaks swagger's validation
-        // With this we can override that parser to return a string
-        types.setTypeParser(TIMESTAMPZ, (dateString) => dateString);
+        setCustomParsers();
 
         instance = knex.default(knexConfig);
       }
