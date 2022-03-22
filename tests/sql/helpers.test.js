0d4e3090b1c494d8701d6e8840ecaaf0bd565f71
cosa65 <martin@biomage.net>
2022-03-22 18:38:19 -0300

    Add tests for sql helpers

---
+++ tests/sql/helpers.test.js
@@ -0,0 +1,59 @@
+const { sqlToCamelCased, aggregateIntoJson } = require('../../src/sql/helpers');
+
+describe('sqlToCamelCased', () => {
+  it('Works correctly', () => {
+    const experimentFields = [
+      'id',
+      'name',
+      'description',
+      'samples_order',
+      'notify_by_email',
+      'created_at',
+      'updated_at',
+    ];
+
+    expect(sqlToCamelCased(experimentFields)).toEqual([
+      'id as id',
+      'name as name',
+      'description as description',
+      'samples_order as samplesOrder',
+      'notify_by_email as notifyByEmail',
+      'created_at as createdAt',
+      'updated_at as updatedAt',
+    ]);
+  });
+});
+
+describe('aggregateIntoJson', () => {
+  it('Works correctly', async () => {
+    const mockGroupBy = jest.fn(() => Promise.resolve('finish'));
+    const mockFrom = jest.fn(() => ({ groupBy: mockGroupBy }));
+    const mockSelect = jest.fn(() => ({ from: mockFrom }));
+
+    const mockJsonbObjectAggResult = 'jsonb_object_agg(etc, etc)';
+    const mockSql = {
+      raw: jest.fn(() => mockJsonbObjectAggResult),
+      select: mockSelect,
+    };
+
+    const result = await aggregateIntoJson(
+      'originalQuery',
+      ['root_1', 'root_2'],
+      ['nested_1', 'nested_2'],
+      'aggregationColumnKey',
+      'aggregationJsonKey',
+      mockSql,
+    );
+
+    expect(mockSql.raw)
+      .toHaveBeenCalledWith(
+        'jsonb_object_agg(aggregationColumnKey, json_build_object(\'nested1\', nested_1, \'nested2\', nested_2)) as aggregationJsonKey',
+      );
+
+    expect(mockSelect).toHaveBeenCalledWith(['root_1 as root1', 'root_2 as root2', mockJsonbObjectAggResult]);
+    expect(mockFrom).toHaveBeenCalledWith('originalQuery');
+    expect(mockGroupBy).toHaveBeenCalledWith(['root_1', 'root_2']);
+
+    expect(result).toEqual('finish');
+  });
+});
