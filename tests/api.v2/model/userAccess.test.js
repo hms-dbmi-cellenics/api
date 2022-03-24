b287d30b9fc400a8e4474128c7bc343a32e2ebdb
cosa65 <martin@biomage.net>
2022-03-24 19:32:10 -0300

    Remove unnecessary checking in userAccess test

--- tests/api.v2/model/userAccess.test.js
+++ tests/api.v2/model/userAccess.test.js
@@ -1,5 +1,3 @@
-const generateBasicModelFunctions = require('../../../src/api.v2/helpers/generateBasicModelFunctions');
-
 jest.mock('../../../src/api.v2/helpers/generateBasicModelFunctions',
   () => jest.fn(() => ({ hasFakeBasicModelFunctions: true })));
 
@@ -7,17 +5,6 @@ const userAccess = require('../../../src/api.v2/model/userAccess');
 
 describe('model/userAccess', () => {
   it('Returns the correct generateBasicModelFunctions', async () => {
-    expect(generateBasicModelFunctions).toHaveBeenCalledTimes(1);
-    expect(generateBasicModelFunctions).toHaveBeenCalledWith({
-      tableName: 'user_access',
-      selectableProps: [
-        'user_id',
-        'experiment_id',
-        'access_role',
-        'updated_at',
-      ],
-    });
-
     expect(userAccess).toEqual(
       expect.objectContaining({
         hasFakeBasicModelFunctions: true,
