efc35cbb93163bd87fb536381a977f36184ab101
cosa65 <martin@biomage.net>
2022-03-24 22:22:42 -0300

    Add some tests for experiment routes

--- tests/api.v2/routes/experiment.test.js
+++ tests/api.v2/routes/experiment.test.js
@@ -1,8 +1,21 @@
+// @ts-nocheck
 const express = require('express');
 const request = require('supertest');
 const expressLoader = require('../../../src/loaders/express');
 
-jest.mock('../../../src/api.v2/controllers/experimentController');
+const { OK } = require('../../../src/utils/responses');
+
+const experimentController = require('../../../src/api.v2/controllers/experimentController');
+
+const getExperimentResponse = require('../mocks/data/getExperimentResponse');
+
+jest.mock('../../../src/api.v2/controllers/experimentController', () => ({
+  createExperiment: jest.fn(),
+  getExperiment: jest.fn(),
+  patchExperiment: jest.fn(),
+  updateSamplePosition: jest.fn(),
+}));
+
 jest.mock('../../../src/utils/authMiddlewares');
 
 describe('tests for experiment route', () => {
@@ -22,6 +35,8 @@ describe('tests for experiment route', () => {
   });
 
   it('Creating a new experiment results in a successful response', async (done) => {
+    experimentController.createExperiment.mockImplementationOnce((req, res) => res.json(OK()));
+
     const experimentId = 'experiment-id';
 
     const newExperimentData = {
@@ -45,6 +60,8 @@ describe('tests for experiment route', () => {
   });
 
   it('Creating a new experiment fails if body is invalid', async (done) => {
+    experimentController.createExperiment.mockImplementationOnce((req, res) => res.json(OK()));
+
     const experimentId = 'experiment-id';
 
     const invalidExperimentData = {
@@ -54,6 +71,119 @@ describe('tests for experiment route', () => {
     request(app)
       .post(`/v2/experiments/${experimentId}`)
       .send(invalidExperimentData)
+      .expect(400)
+      .end((err) => {
+        if (err) {
+          return done(err);
+        }
+        // there is no point testing for the values of the response body
+        // - if something is wrong, the schema validator will catch it
+        return done();
+      });
+  });
+
+  it('getExperiment results in a successful response', async (done) => {
+    const experimentId = 'experiment-id';
+
+    experimentController.getExperiment.mockImplementationOnce(
+      (req, res) => res.json(getExperimentResponse),
+    );
+
+    request(app)
+      .get(`/v2/experiments/${experimentId}`)
+      .expect(200)
+      .end((err) => {
+        if (err) {
+          return done(err);
+        }
+        // there is no point testing for the values of the response body
+        // - if something is wrong, the schema validator will catch it
+        return done();
+      });
+  });
+
+  it('patchExperiment valid body works', async (done) => {
+    const experimentId = 'experiment-id';
+
+    const body = {
+      notifyByEmail: true,
+    };
+
+    experimentController.patchExperiment.mockImplementationOnce((req, res) => res.json(OK()));
+
+    request(app)
+      .patch(`/v2/experiments/${experimentId}`)
+      .send(body)
+      .expect(200)
+      .end((err) => {
+        if (err) {
+          return done(err);
+        }
+        // there is no point testing for the values of the response body
+        // - if something is wrong, the schema validator will catch it
+        return done();
+      });
+  });
+
+  it('patchExperiment invalid body fails 400', async (done) => {
+    const experimentId = 'experiment-id';
+
+    const body = {
+      notifyByEmail: 'true',
+    };
+
+    experimentController.patchExperiment.mockImplementationOnce((req, res) => res.json(OK()));
+
+    request(app)
+      .patch(`/v2/experiments/${experimentId}`)
+      .send(body)
+      .expect(400)
+      .end((err) => {
+        if (err) {
+          return done(err);
+        }
+        // there is no point testing for the values of the response body
+        // - if something is wrong, the schema validator will catch it
+        return done();
+      });
+  });
+
+  it('updateSamplePosition valid body works', async (done) => {
+    const experimentId = 'experiment-id';
+
+    const body = {
+      oldPosition: 4,
+      newPosition: 1,
+    };
+
+    experimentController.updateSamplePosition.mockImplementationOnce((req, res) => res.json(OK()));
+
+    request(app)
+      .put(`/v2/experiments/${experimentId}/samples/position`)
+      .send(body)
+      .expect(200)
+      .end((err) => {
+        if (err) {
+          return done(err);
+        }
+        // there is no point testing for the values of the response body
+        // - if something is wrong, the schema validator will catch it
+        return done();
+      });
+  });
+
+  it('updateSamplePosition invalid body fails 400', async (done) => {
+    const experimentId = 'experiment-id';
+
+    const body = {
+      oldPosition: 4,
+    };
+
+    experimentController.updateSamplePosition.mockImplementationOnce((req, res) => res.json(OK()));
+
+    request(app)
+      .put(`/v2/experiments/${experimentId}/samples/position`)
+      .send(body)
       .expect(400)
       .end((err) => {
         if (err) {
