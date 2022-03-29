5572cefc4764558040253154e22a8cd9774fc99c
cosa65 <martin@biomage.net>
2022-03-29 10:52:12 -0300

    Change to using first()

--- src/api.v2/model/userAccess.js
+++ src/api.v2/model/userAccess.js
@@ -1,3 +1,5 @@
+const _ = require('lodash');
+
 const generateBasicModelFunctions = require('../helpers/generateBasicModelFunctions');
 const sqlClient = require('../../sql/sqlClient');
 
@@ -18,17 +20,19 @@ const basicModelFunctions = generateBasicModelFunctions({
 });
 
 const canAccessExperiment = async (userId, experimentId, url, method) => {
-  const results = await sqlClient.get()
-    .select()
-    .from(userAccessTable)
-    .where({ experiment_id: experimentId, user_id: userId });
+  const sql = sqlClient.get();
+
+  const result = await sql(userAccessTable)
+    .first()
+    .where({ experiment_id: experimentId, user_id: userId })
+    .from(userAccessTable);
 
   // If there is no entry for this user and role, then user definitely doesn't have access
-  if (results.length === 0) {
+  if (_.isNil(result)) {
     return false;
   }
 
-  const { access_role: role } = results[0];
+  const { access_role: role } = result[0];
 
   return isRoleAuthorized(role, url, method);
 };
