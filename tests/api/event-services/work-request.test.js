0b62c2f00802a3eec8f4407b14ddea9fb2c1e945
Anugerah Erlaut <aerlaut@live.com>
2022-04-11 13:39:34 +0100

    update error message

--- tests/api/event-services/work-request.test.js
+++ tests/api/event-services/work-request.test.js
@@ -74,7 +74,7 @@ describe('handleWorkRequest', () => {
       await handleWorkRequest(workRequest);
     } catch (e) {
       expect(e.message).toMatch(
-        /^Error: distanceMetric must be one of the following/,
+        /^Error: distanceMetric is not set to an allowed value.*/,
       );
     }
   });
