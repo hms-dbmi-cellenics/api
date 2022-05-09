7b3c660e1092ed3d778b97627c69f887a8de67a4
Pol Alvarez <pol.avms@gmail.com>
2022-05-09 16:37:55 +0200

    fixed wrong test name

--- tests/api.v2/routes/kubernetes.test.js
+++ tests/api.v2/routes/kubernetes.test.js
@@ -67,7 +67,7 @@ const express = require('express');
 const request = require('supertest');
 const expressLoader = require('../../../src/loaders/express');
 
-describe('tests for experiment route', () => {
+describe('tests for kubernetes route', () => {
   let app = null;
 
   beforeEach(async () => {
