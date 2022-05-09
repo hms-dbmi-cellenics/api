7d59965b7fdcbbbef8c49791cb02c05505e57815
Anugerah Erlaut <aerlaut@live.com>
2022-05-09 12:20:22 +0100

    add controller test

--- tests/api.v2/controllers/accessController.test.js
+++ tests/api.v2/controllers/accessController.test.js
@@ -1,8 +1,15 @@
 // @ts-nocheck
-const getExperimentUsers = require('../../../src/api.v2/helpers/access/getExperimentUsers');
 const userAccessController = require('../../../src/api.v2/controllers/accessController');
+const getExperimentUsers = require('../../../src/api.v2/helpers/access/getExperimentUsers');
+const createUserInvite = require('../../../src/api.v2/helpers/access/createUserInvite');
+const removeAccess = require('../../../src/api.v2/helpers/access/removeAccess');
+
+const OK = require('../../../src/utils/responses/OK');
+const AccessRole = require('../../../src/utils/enums/AccessRole');
 
 jest.mock('../../../src/api.v2/helpers/access/getExperimentUsers');
+jest.mock('../../../src/api.v2/helpers/access/createUserInvite');
+jest.mock('../../../src/api.v2/helpers/access/removeAccess');
 
 const mockRes = {
   json: jest.fn(),
@@ -37,5 +44,42 @@ describe('accessController', () => {
 
     expect(getExperimentUsers).toHaveBeenCalledWith('mockExperimentId');
     expect(mockRes.json).toHaveBeenCalledWith(mockUsersList);
+  });
+
+  it('inviteUser works correctly', async () => {
+    const mockReq = {
+      user: { email: 'owner@example.com' },
+      params: { experimentId: 'mockExperimentId' },
+      body: { userEmail: 'user@example.com', role: AccessRole.ADMIN },
+    };
+
+    createUserInvite.mockImplementationOnce(
+      () => Promise.resolve(),
+    );
+
+    await userAccessController.inviteUser(mockReq, mockRes);
+
+    const callParams = createUserInvite.mock.calls[0];
+
+    expect(callParams).toMatchSnapshot();
+    expect(mockRes.json).toHaveBeenCalledWith(OK());
+  });
+
+  it('revokeAccess works correctly', async () => {
+    const mockReq = {
+      params: { experimentId: 'mockExperimentId' },
+      body: { userEmail: 'user@example.com' },
+    };
+
+    removeAccess.mockImplementationOnce(
+      () => Promise.resolve(),
+    );
+
+    await userAccessController.revokeAccess(mockReq, mockRes);
+
+    const callParams = removeAccess.mock.calls[0];
+
+    expect(callParams).toMatchSnapshot();
+    expect(mockRes.json).toHaveBeenCalledWith(OK());
   });
 });
