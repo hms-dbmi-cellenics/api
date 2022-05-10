37fbdfac00872dbf37d54d8e3367e6fee95d0b38
Anugerah Erlaut <aerlaut@live.com>
2022-05-10 13:29:10 +0100

    Revert "[BIOMAGE-1878] - Add v2 cellsets endpoints"

--- src/api.v2/helpers/s3/bucketNames.js
+++ src/api.v2/helpers/s3/bucketNames.js
@@ -2,7 +2,6 @@ const config = require('../../../config');
 
 const bucketNames = {
   SAMPLE_FILES: `biomage-originals-${config.clusterEnv}`,
-  CELL_SETS: `cell-sets-${config.clusterEnv}`,
 };
 
 module.exports = bucketNames;
