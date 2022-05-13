4687a711f73266d9148ca2766ae2eabe8551ae47
Anugerah Erlaut <aerlaut@live.com>
2022-05-13 11:56:58 +0100

    add tests for user access model

--- src/api.v2/model/__mocks__/UserAccess.js
+++ src/api.v2/model/__mocks__/UserAccess.js
@@ -1,9 +1,13 @@
 const BasicModel = require('./BasicModel')();
 
 const stub = {
+  getExperimentUsers: jest.fn(),
+  addToInviteAccess: jest.fn(),
+  registerNewUserAccess: jest.fn(),
+  grantAccess: jest.fn(),
+  removeAccess: jest.fn(),
   createNewExperimentPermissions: jest.fn(),
   canAccessExperiment: jest.fn(),
-  getExperimentUsers: jest.fn(),
   ...BasicModel,
 };
 
--- tests/api.v2/model/UserAccess.test.js
+++ tests/api.v2/model/UserAccess.test.js
@@ -1,5 +1,6 @@
 // @ts-nocheck
 const roles = require('../../../src/api.v2/helpers/roles');
+const testConfig = require('../../../src/config/test-config');
 
 const { mockSqlClient } = require('../mocks/getMockSqlClient')();
 
@@ -22,15 +23,17 @@ const BasicModel = require('../../../src/api.v2/model/BasicModel');
 const UserAccess = require('../../../src/api.v2/model/UserAccess');
 const AccessRole = require('../../../src/utils/enums/AccessRole');
 
-const mockUserId = '1234-5678-9012-3456';
+const mockAdminUserId = testConfig.adminSub;
+const mockUserId = '1234-5678-9012-1234';
+const mockUserEmail = `${mockUserId}@example.com`;
 const mockExperimentId = 'experimentId';
 const mockRole = 'mockRole';
 
 const mockUserAccessCreateResults = [
   [{
-    userId: 'mockAdminSub',
+    userId: mockAdminUserId,
     experimentId: mockExperimentId,
-    accessRole: AccessRole.OWNER,
+    accessRole: AccessRole.ADMIN,
     updatedAt: '1910-03-23 21:06:00.573142+00',
   }],
   [{
@@ -41,9 +44,9 @@ const mockUserAccessCreateResults = [
   }],
 ];
 
-const mockGetUserAccessResults = [
+const mockGetExperimentUsersResults = [
   {
-    userId: 'mockAdminSub',
+    userId: mockAdminUserId,
     experimentId: mockExperimentId,
     accessRole: AccessRole.ADMIN,
     updatedAt: '1910-03-23 21:06:00.573142+00',
@@ -51,7 +54,7 @@ const mockGetUserAccessResults = [
   {
     userId: mockUserId,
     experimentId: mockExperimentId,
-    accessRole: 'owner',
+    accessRole: AccessRole.OWNER,
     updatedAt: '1910-03-23 21:06:00.573142+00',
   },
 ];
@@ -61,32 +64,63 @@ describe('model/userAccess', () => {
     jest.clearAllMocks();
   });
 
-  it('getUserAccess work correctly', async () => {
-    const experimentId = 'experimentId';
-
+  it('getExperimentUsers work correctly', async () => {
     const mockFind = jest.spyOn(BasicModel.prototype, 'find')
-      .mockImplementationOnce(() => Promise.resolve(mockGetUserAccessResults));
+      .mockImplementationOnce(() => Promise.resolve(mockGetExperimentUsersResults));
 
-    const result = await new UserAccess().getExperimentUsers(experimentId);
+    const result = await new UserAccess().getExperimentUsers(mockExperimentId);
 
-    expect(mockFind).toHaveBeenCalledWith({ experiment_id: experimentId });
+    expect(mockFind).toHaveBeenCalledWith({ experiment_id: mockExperimentId });
     expect(mockFind).toHaveBeenCalledTimes(1);
 
     expect(result).toMatchSnapshot();
   });
 
-  it('getUserAccess throws a not found error if experiment does not exist', async () => {
-    const experimentId = 'experimentId';
-
+  it('getExperimentUsers throws a not found error if experiment does not exist', async () => {
     const mockFind = jest.spyOn(BasicModel.prototype, 'find')
       .mockImplementationOnce(() => Promise.resolve([]));
 
     await expect(
-      new UserAccess().getExperimentUsers(experimentId),
+      new UserAccess().getExperimentUsers(mockExperimentId),
     ).rejects.toThrow('Experiment not found');
 
-    expect(mockFind).toHaveBeenCalledWith({ experiment_id: experimentId });
+    expect(mockFind).toHaveBeenCalledWith({ experiment_id: mockExperimentId });
     expect(mockFind).toHaveBeenCalledTimes(1);
+  });
+
+  it('addToInvite access works correctly', async () => {
+    await new UserAccess().addToInviteAccess(mockUserEmail, mockUserId, mockRole);
+
+    expect(mockSqlClient.insert.mock.calls).toMatchSnapshot('insertParams');
+    expect(mockSqlClient.into.mock.calls).toMatchSnapshot('intoParams');
+  });
+
+  it('registerNewUserAccess access works correctly', async () => {
+    const mockInvitation = [{
+      experiment_id: 'mockExperiment1',
+      access_role: mockRole,
+    },
+    {
+      user_id: 'mockExperiment2',
+      access_role: mockRole,
+    }];
+
+    mockSqlClient.where.mockImplementationOnce(() => mockInvitation);
+
+    await new UserAccess().registerNewUserAccess(mockUserEmail, mockUserId);
+
+    // Gets the correct invitation access
+    expect(mockSqlClient.select.mock.calls[0]).toMatchSnapshot('selectParams');
+    expect(mockSqlClient.from.mock.calls[0]).toMatchSnapshot('fromParams');
+    expect(mockSqlClient.where.mock.calls[0]).toMatchSnapshot('whereParams');
+
+    // Inserts the user records
+    expect(mockSqlClient.insert.mock.calls[0]).toMatchSnapshot('insertParams');
+
+    // Deletes the invitation record
+    expect(mockSqlClient.del).toHaveBeenCalledTimes(1);
+    expect(mockSqlClient.from.mock.calls[1]).toMatchSnapshot('fromParams');
+    expect(mockSqlClient.where.mock.calls[1]).toMatchSnapshot('whereParams');
   });
 
   it('grantAccess work correctly', async () => {
@@ -119,64 +153,52 @@ describe('model/userAccess', () => {
   });
 
   it('createNewExperimentPermissions works correctly', async () => {
-    const userId = 'userId';
-    const experimentId = 'experimentId';
-
     const mockCreate = jest.spyOn(BasicModel.prototype, 'create')
       .mockImplementationOnce(() => Promise.resolve([mockUserAccessCreateResults[0]]))
       .mockImplementationOnce(() => Promise.resolve([mockUserAccessCreateResults[1]]));
 
-    await new UserAccess().createNewExperimentPermissions(userId, experimentId);
+    await new UserAccess().createNewExperimentPermissions(mockUserId, mockExperimentId);
 
-    expect(mockCreate).toHaveBeenCalledWith({ access_role: 'admin', experiment_id: 'experimentId', user_id: 'mockAdminSub' });
-    expect(mockCreate).toHaveBeenCalledWith({ access_role: 'owner', experiment_id: 'experimentId', user_id: 'userId' });
+    console.log('*** test', mockCreate.mock.calls);
+
+    expect(mockCreate).toHaveBeenCalledWith({ access_role: roles.ADMIN, experiment_id: mockExperimentId, user_id: mockAdminUserId });
+    expect(mockCreate).toHaveBeenCalledWith({ access_role: roles.OWNER, experiment_id: mockExperimentId, user_id: mockUserId });
     expect(mockCreate).toHaveBeenCalledTimes(2);
   });
 
   it('createNewExperimentPermissions works correctly when creator is admin', async () => {
-    const userId = 'mockAdminSub';
-    const experimentId = 'experimentId';
-
     const mockCreate = jest.spyOn(BasicModel.prototype, 'create')
       .mockImplementationOnce(() => Promise.resolve([mockUserAccessCreateResults[0]]));
 
-    await new UserAccess().createNewExperimentPermissions(userId, experimentId);
+    await new UserAccess().createNewExperimentPermissions(mockAdminUserId, mockExperimentId);
 
-    expect(mockCreate).toHaveBeenCalledWith({ access_role: 'admin', experiment_id: 'experimentId', user_id: 'mockAdminSub' });
+    expect(mockCreate).toHaveBeenCalledWith({ access_role: roles.ADMIN, experiment_id: mockExperimentId, user_id: mockAdminUserId });
     expect(mockCreate).toHaveBeenCalledTimes(1);
   });
 
   it('createNewExperimentPermissions fails if admin creation failed', async () => {
-    const userId = 'userId';
-    const experimentId = 'experimentId';
-
     const mockCreate = jest.spyOn(BasicModel.prototype, 'create')
       .mockImplementationOnce(() => Promise.reject(new Error('A happy sql error :)')));
 
-    await expect(new UserAccess().createNewExperimentPermissions(userId, experimentId)).rejects.toThrow('A happy sql error :)');
+    await expect(new UserAccess().createNewExperimentPermissions(mockUserId, mockExperimentId)).rejects.toThrow('A happy sql error :)');
 
-    expect(mockCreate).toHaveBeenCalledWith({ access_role: 'admin', experiment_id: 'experimentId', user_id: 'mockAdminSub' });
+    expect(mockCreate).toHaveBeenCalledWith({ access_role: roles.ADMIN, experiment_id: mockExperimentId, user_id: mockAdminUserId });
     expect(mockCreate).toHaveBeenCalledTimes(1);
   });
 
   it('createNewExperimentPermissions fails if owner creation failed', async () => {
-    const userId = 'userId';
-    const experimentId = 'experimentId';
-
     const mockCreate = jest.spyOn(BasicModel.prototype, 'create')
       .mockImplementationOnce(() => Promise.resolve([mockUserAccessCreateResults[0]]))
       .mockImplementationOnce(() => Promise.reject(new Error('A happy sql error :)')));
 
-    await expect(new UserAccess().createNewExperimentPermissions(userId, experimentId)).rejects.toThrow('A happy sql error :)');
+    await expect(new UserAccess().createNewExperimentPermissions(mockUserId, mockExperimentId)).rejects.toThrow('A happy sql error :)');
 
-    expect(mockCreate).toHaveBeenCalledWith({ access_role: 'admin', experiment_id: 'experimentId', user_id: 'mockAdminSub' });
-    expect(mockCreate).toHaveBeenCalledWith({ access_role: 'owner', experiment_id: 'experimentId', user_id: 'userId' });
+    expect(mockCreate).toHaveBeenCalledWith({ access_role: roles.ADMIN, experiment_id: mockExperimentId, user_id: mockAdminUserId });
+    expect(mockCreate).toHaveBeenCalledWith({ access_role: roles.OWNER, experiment_id: mockExperimentId, user_id: mockUserId });
     expect(mockCreate).toHaveBeenCalledTimes(2);
   });
 
   it('canAccessExperiment allows access if an entry exists and isRoleAuthorized allows', async () => {
-    const userId = 'userId';
-    const experimentId = 'experimentId';
     const url = 'url';
     const method = 'method';
 
@@ -184,13 +206,13 @@ describe('model/userAccess', () => {
 
     roles.isRoleAuthorized.mockImplementationOnce(() => true);
 
-    const result = await new UserAccess().canAccessExperiment(userId, experimentId, url, method);
+    const result = await new UserAccess().canAccessExperiment(mockUserId, mockExperimentId, url, method);
 
 
     expect(mockSqlClient.first).toHaveBeenCalled();
     expect(mockSqlClient.from).toHaveBeenCalledWith('user_access');
     expect(mockSqlClient.where).toHaveBeenCalledWith(
-      { experiment_id: experimentId, user_id: userId },
+      { experiment_id: mockExperimentId, user_id: mockUserId },
     );
 
     expect(roles.isRoleAuthorized).toHaveBeenCalledWith('roleThatIsOk', url, method);
@@ -198,19 +220,17 @@ describe('model/userAccess', () => {
   });
 
   it('canAccessExperiment denies access if no entry exists', async () => {
-    const userId = 'userId';
-    const experimentId = 'experimentId';
     const url = 'url';
     const method = 'method';
 
     mockSqlClient.from.mockImplementationOnce(() => undefined);
 
-    const result = await new UserAccess().canAccessExperiment(userId, experimentId, url, method);
+    const result = await new UserAccess().canAccessExperiment(mockUserId, mockExperimentId, url, method);
 
     expect(mockSqlClient.first).toHaveBeenCalled();
     expect(mockSqlClient.from).toHaveBeenCalledWith('user_access');
     expect(mockSqlClient.where).toHaveBeenCalledWith(
-      { experiment_id: experimentId, user_id: userId },
+      { experiment_id: mockExperimentId, user_id: mockUserId },
     );
 
     expect(roles.isRoleAuthorized).not.toHaveBeenCalled();
@@ -219,8 +239,6 @@ describe('model/userAccess', () => {
   });
 
   it('canAccessExperiment denies access if an entry exists but the role doesn\'t allow this kind of access', async () => {
-    const userId = 'userId';
-    const experimentId = 'experimentId';
     const url = 'url';
     const method = 'method';
 
@@ -228,12 +246,12 @@ describe('model/userAccess', () => {
 
     roles.isRoleAuthorized.mockImplementationOnce(() => false);
 
-    const result = await new UserAccess().canAccessExperiment(userId, experimentId, url, method);
+    const result = await new UserAccess().canAccessExperiment(mockUserId, mockExperimentId, url, method);
 
     expect(mockSqlClient.first).toHaveBeenCalled();
     expect(mockSqlClient.from).toHaveBeenCalledWith('user_access');
     expect(mockSqlClient.where).toHaveBeenCalledWith(
-      { experiment_id: experimentId, user_id: userId },
+      { experiment_id: mockExperimentId, user_id: mockUserId },
     );
 
     expect(roles.isRoleAuthorized).toHaveBeenCalledWith('roleThatIsNotOk', url, method);
