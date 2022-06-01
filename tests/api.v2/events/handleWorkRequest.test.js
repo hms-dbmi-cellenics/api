ca7396e3aea0336472b25391d4f3131646bc3314
stefanbabukov <stefanbabukov98@gmail.com>
2022-06-01 10:43:36 +0100

    comment fix and added another test

--- tests/api.v2/events/handleWorkRequest.test.js
+++ tests/api.v2/events/handleWorkRequest.test.js
@@ -1,15 +1,11 @@
 const handleWorkRequest = require('../../../src/api.v2/events/handleWorkRequest');
+const validateAndSubmitWork = require('../../../src/api.v2/events/validateAndSubmitWork');
 const fake = require('../../test-utils/constants');
 
 jest.mock('../../../src/api.v2/middlewares/authMiddlewares');
 jest.mock('aws-xray-sdk');
-jest.mock('../../../src/api.v2/events/validateAndSubmitWork', () => (data) => {
-  if (data.uuid === 'invalid') {
-    throw new Error('invalid request');
-  }
-  return jest.fn();
-});
 
+jest.mock('../../../src/api.v2/events/validateAndSubmitWork', () => jest.fn());
 
 const data = {
   uuid: 'someuuid-asd-asd-asdddasa',
@@ -26,19 +22,25 @@ describe('Handle work socket callback', () => {
   });
 
   it('submits work', async () => {
+    validateAndSubmitWork.mockImplementation(() => Promise.resolve());
+
     await handleWorkRequest(mockSocket, data, mockXraySegment);
     expect(mockSocket.emit.mock.calls[0]).toMatchSnapshot();
+    expect(validateAndSubmitWork.mock.calls[0]).toMatchSnapshot();
   });
 
-  it('Failing submits a work-response with error', async () => {
-    data.uuid = 'invalid';
+  it('If the worker fails, the API emits a work-response with error', async () => {
+    validateAndSubmitWork.mockImplementation(() => Promise.reject({ message: 'fail' }));
+
     await handleWorkRequest(mockSocket, data, mockXraySegment);
     expect(mockSocket.emit.mock.calls[0]).toMatchSnapshot();
     expect(mockXraySegment.addError).toHaveBeenCalled();
+    expect(validateAndSubmitWork.mock.calls[0]).toMatchSnapshot();
   });
 
   it('data without authorization also throws an error', async () => {
     delete data.Authorization;
+    validateAndSubmitWork.mockReturnValue(() => Promise.reject());
     await handleWorkRequest(mockSocket, data, mockXraySegment);
     expect(mockSocket.emit.mock.calls[0]).toMatchSnapshot();
   });
