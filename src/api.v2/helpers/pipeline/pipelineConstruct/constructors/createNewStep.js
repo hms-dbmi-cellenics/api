9a761e7c66161515b91e2239aa562dbe23153e41
Pol Alvarez <pol.avms@gmail.com>
2022-06-27 16:34:22 +0200

    revmoved timeout from state machines

--- src/api.v2/helpers/pipeline/pipelineConstruct/constructors/createNewStep.js
+++ src/api.v2/helpers/pipeline/pipelineConstruct/constructors/createNewStep.js
@@ -70,7 +70,7 @@ const createNewStep = (context, step, stepArgs) => {
     Resource: activityArn,
     ResultPath: null,
     TimeoutSeconds: 10800,
-    HeartbeatSeconds: 90,
+    // HeartbeatSeconds: 90,
     Parameters: params,
     ...!step.End && { Next: step.XNextOnCatch || step.Next },
   };
