6f85803de4fd556a2b23faa2a218aa86143bcc78
Anugerah Erlaut <aerlaut@live.com>
2022-07-05 12:04:53 +0100

    Remove test code

--- src/api.v2/helpers/pipeline/hooks/sendNotification.js
+++ src/api.v2/helpers/pipeline/hooks/sendNotification.js
@@ -10,7 +10,7 @@ const Experiment = require('../../../model/Experiment');
 const getLogger = require('../../../../utils/getLogger');
 const sendEmail = require('../../../../utils/sendEmail');
 const sendFailedSlackMessage = require('../../../../utils/send-failed-slack-message');
-// const config = require('../../../../config');
+const config = require('../../../../config');
 const buildPipelineStatusEmailBody = require('../../../../utils/emailTemplates/buildPipelineStatusEmailBody');
 
 const logger = getLogger();
@@ -31,8 +31,7 @@ const sendNotification = async (message) => {
 
   const { status } = statusRes[processName];
 
-  // if (status === FAILED && ['production', 'test'].includes(config.clusterEnv)) {
-  if (status === FAILED) {
+  if (status === FAILED && ['production', 'test'].includes(config.clusterEnv)) {
     try {
       const { stateMachineArn } = experiment.pipelines[processName];
 
