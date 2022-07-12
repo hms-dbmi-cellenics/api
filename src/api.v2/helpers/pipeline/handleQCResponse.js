f402d799f22aa742fdcb50b115c9efa31d241061
stefanbabukov <stefanbabukov98@gmail.com>
2022-07-12 12:15:57 +0300

    checking pipeline response

--- src/api.v2/helpers/pipeline/handleQCResponse.js
+++ src/api.v2/helpers/pipeline/handleQCResponse.js
@@ -60,7 +60,7 @@ const updateProcessingConfigWithQCStep = async (taskName, experimentId, output, 
   // TODO the processing config validation was not being enforced because the 'anyOf' requirement
   // was not being correctly applied. This needs to be refactored together with the
   // pipeline and ideally while unifying the qc & gem2s responses.
-  // await validateRequest(output, 'ProcessingConfigBodies.v1.yaml');
+  await validateRequest(output, 'ProcessingConfigBodies.v2.yaml');
 
   const experiment = new Experiment();
 
