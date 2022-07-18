4dc4a055523b72b2c80b1b2e97b1b00362133daa
cosa65 <martin@biomage.net>
2022-07-18 11:45:58 -0300

    Rever tmp changes, point back to real user pool

--- src/config/default-config.js
+++ src/config/default-config.js
@@ -50,7 +50,7 @@ async function getAwsPoolId() {
   // when k8s is undefined we are in development where we use staging user pool so we set
   // it as the default one.
   const k8sEnv = process.env.K8S_ENV || 'staging';
-  const userPoolName = `test-biomage-user-pool-${k8sEnv}`;
+  const userPoolName = `biomage-user-pool-case-insensitive-${k8sEnv}`;
 
   const poolId = UserPools.find((pool) => pool.Name === userPoolName).Id;
   return poolId;
