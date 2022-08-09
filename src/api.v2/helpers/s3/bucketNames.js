67ee44f5cea143e5d8335a24578a93fd8fc1b217
cosa65 <martin@biomage.net>
2022-08-09 12:04:43 -0300

    Switch to using -test buckets

--- src/api.v2/helpers/s3/bucketNames.js
+++ src/api.v2/helpers/s3/bucketNames.js
@@ -1,13 +1,13 @@
 const config = require('../../../config');
 
 const bucketNames = {
-  SAMPLE_FILES: `biomage-originals-${config.clusterEnv}-${config.awsAccountId}`,
-  PROCESSED_MATRIX: `processed-matrix-${config.clusterEnv}-${config.awsAccountId}`,
-  RAW_SEURAT: `biomage-source-${config.clusterEnv}-${config.awsAccountId}`,
-  CELL_SETS: `cell-sets-${config.clusterEnv}-${config.awsAccountId}`,
-  FILTERED_CELLS: `biomage-filtered-cells-${config.clusterEnv}-${config.awsAccountId}`,
-  WORKER_RESULTS: `worker-results-${config.clusterEnv}-${config.awsAccountId}`,
-  PLOTS: `plots-tables-${config.clusterEnv}-${config.awsAccountId}`,
+  SAMPLE_FILES: `biomage-originals-test-${config.clusterEnv}-${config.awsAccountId}`,
+  PROCESSED_MATRIX: `processed-matrix-test-${config.clusterEnv}-${config.awsAccountId}`,
+  RAW_SEURAT: `biomage-source-test-${config.clusterEnv}-${config.awsAccountId}`,
+  CELL_SETS: `cell-sets-test-${config.clusterEnv}-${config.awsAccountId}`,
+  FILTERED_CELLS: `biomage-filtered-cells-test-${config.clusterEnv}-${config.awsAccountId}`,
+  WORKER_RESULTS: `worker-results-test-${config.clusterEnv}-${config.awsAccountId}`,
+  PLOTS: `plots-tables-test-${config.clusterEnv}-${config.awsAccountId}`,
 };
 
 module.exports = bucketNames;
