cbd6212cffe40ec0557a6a44e8b90dfaa40ebd9f
cosa65 <martin@biomage.net>
2022-08-10 08:41:41 -0300

    Add sql migration to set the new qc rerun flag in experiment tables

---
+++ src/sql/migrations/20220809193702_add_disabled_qc_rerun_attribute.js
@@ -0,0 +1,19 @@
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.up = async (knex) => {
+  await knex.schema.alterTable('experiment', (table) => {
+    table.boolean('qc_rerun_disabled').notNullable().defaultTo(false);
+  });
+};
+
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.down = async (knex) => {
+  await knex.schema.alterTable('experiment', (table) => {
+    table.dropColumn('qc_rerun_disabled');
+  });
+};
