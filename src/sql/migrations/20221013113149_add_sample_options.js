f1b3b0c444a1265b3c1d4820303c8466e4d2a882
Anugerah Erlaut <aerlaut@live.com>
2022-10-13 15:11:34 +0100

    add options column in sample table

---
+++ src/sql/migrations/20221013113149_add_sample_options.js
@@ -0,0 +1,19 @@
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.up = async (knex) => {
+  await knex.schema.alterTable('sample', (table) => {
+    table.jsonb('options');
+  });
+};
+
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.down = async (knex) => {
+  await knex.schema.alterTable('sample', (table) => {
+    table.dropColumn('options');
+  });
+};
