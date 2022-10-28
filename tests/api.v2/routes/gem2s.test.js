35a43b80d6118f037e83e49962fc2c633540a33a
ivababukova <iva@biomage.net>
2022-10-28 18:17:12 +0200

    fix tests

--- tests/api.v2/routes/gem2s.test.js
+++ tests/api.v2/routes/gem2s.test.js
@@ -77,4 +77,43 @@ describe('tests for gem2s route', () => {
         return done();
       });
   });
+
+  it('Sending a gem2sResult results in a successful response', async (done) => {
+    gem2sController.handleResponse.mockImplementationOnce((req, res) => {
+      res.json(OK());
+      return Promise.resolve();
+    });
+
+    const mockBody = JSON.stringify({
+      Type: 'Notification',
+      MessageId: 'ce0a05bf-c500-4dc7-8d0d-2ba974bf2831',
+      TopicArn: 'arn:aws:sns:eu-west-1:000000000000:work-results-development-default-v2',
+      Message: '{a: "the actual message"}',
+      Timestamp: '2022-05-10T17:03:16.542Z',
+      SignatureVersion: '1',
+      Signature: 'EXAMPLEpH+..',
+      SigningCertURL: 'https://sns.us-east-1.amazonaws.com/SimpleNotificationService-0000000000000000000000.pem',
+      UnsubscribeURL: 'http://localhost:4566/?Action=Unsubscribe&SubscriptionArn=arn:aws:sns:eu-west-1:000000000000:work-results-development-default-v2:18c4de8d-e180-485a-9b79-de0b173b7db8',
+      MessageAttributes: {
+        type: {
+          Type: 'String',
+          Value: 'GEM2SResponse',
+        },
+      },
+    });
+
+    request(app)
+      .post('/v2/gem2sResults')
+      .set({ 'Content-Type': 'text/plain' })
+      .send(mockBody)
+      .expect(200)
+      .end((err) => {
+        if (err) {
+          return done(err);
+        }
+        // there is no point testing for the values of the response body
+        // - if something is wrong, the schema validator will catch it
+        return done();
+      });
+  });
 });
--- tests/api.v2/routes/qc.test.js
+++ tests/api.v2/routes/qc.test.js
@@ -19,21 +19,6 @@ jest.mock('../../../src/api.v2/middlewares/authMiddlewares');
 
 const experimentId = 'mockExperimentId';
 
-const basicMsg = {
-  MessageId: 'da8827d4-ffc2-5efb-82c1-70f929b2081d',
-  ResponseMetadata: {
-    RequestId: '826314a1-e99f-5fe7-b845-438c3fef9901',
-    HTTPStatusCode: 200,
-    HTTPHeaders: {
-      'x-amzn-requestid': '826314a1-e99f-5fe7-b845-438c3fef9901',
-      'content-type': 'text/xml',
-      'content-length': '294',
-      date: 'Thu, 07 May 2020 09:26:08 GMT',
-    },
-    RetryAttempts: 0,
-  },
-};
-
 describe('PipelineResults route', () => {
   let app = null;
 
@@ -51,6 +36,42 @@ describe('PipelineResults route', () => {
     request(app)
       .post(`/v2/experiments/${experimentId}/qc`)
       .send()
+      .expect(200)
+      .end((err) => {
+        if (err) {
+          return done(err);
+        }
+        // there is no point testing for the values of the response body
+        // - if something is wrong, the schema validator will catch it
+        return done();
+      });
+  });
+
+  it('Sending a qc result returns in a successful response', async (done) => {
+    qcController.handleResponse.mockImplementationOnce((req, res) => {
+      res.json(OK());
+      return Promise.resolve();
+    });
+
+    const basicMsg = JSON.stringify({
+      MessageId: 'da8827d4-ffc2-5efb-82c1-70f929b2081d',
+      ResponseMetadata: {
+        RequestId: '826314a1-e99f-5fe7-b845-438c3fef9901',
+        HTTPStatusCode: 200,
+        HTTPHeaders: {
+          'x-amzn-requestid': '826314a1-e99f-5fe7-b845-438c3fef9901',
+          'content-type': 'text/xml',
+          'content-length': '294',
+          date: 'Thu, 07 May 2020 09:26:08 GMT',
+        },
+        RetryAttempts: 0,
+      },
+    });
+
+    request(app)
+      .post('/v2/pipelineResults')
+      .set({ 'Content-Type': 'text/plain' })
+      .send(_.cloneDeep(basicMsg))
       .expect(200)
       .end((err) => {
         if (err) {
