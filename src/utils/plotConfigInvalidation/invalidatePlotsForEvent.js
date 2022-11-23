9e747d8f09cbe1567dda9cb0057fbbfa6db53509
cosa65 <martin@biomage.net>
2022-11-23 14:10:49 -0300

    Some refactoring

--- src/utils/plotConfigInvalidation/invalidatePlotsForEvent.js
+++ src/utils/plotConfigInvalidation/invalidatePlotsForEvent.js
@@ -51,76 +51,34 @@ const invalidateMatchingPlots = async (experimentId, { plotIdMatcher, keys }) =>
   await new Plot().invalidateAttributesForMatches(experimentId, plotIdMatcher, keys)
 );
 
-const invalidateCategoricalEmbedding = async (experimentId) => (
-  await invalidateMatchingPlots(experimentId, plots.CATEGORICAL_EMBEDDING)
-);
-
-const invalidateFrequencyPlot = async (experimentId) => (
-  await invalidateMatchingPlots(experimentId, plots.FREQUENCY_PLOT)
-);
-
-const invalidateTrajectoryAnalysis = async (experimentId) => (
-  await invalidateMatchingPlots(experimentId, plots.TRAJECTORY_ANALYSIS)
-);
-
-const invalidateContinuousEmbedding = async (experimentId) => (
-  await invalidateMatchingPlots(experimentId, plots.CONTINUOUS_EMBEDDING)
-);
-
-const invalidateMarkerHeatmapPlot = async (experimentId) => (
-  await invalidateMatchingPlots(experimentId, plots.MARKER_HEATMAP)
-);
-
-const invalidateCustomHeatmapPlot = async (experimentId) => (
-  await invalidateMatchingPlots(experimentId, plots.CUSTOM_HEATMAP)
-);
-
-const invalidateViolinPlot = async (experimentId) => (
-  await invalidateMatchingPlots(experimentId, plots.VIOLIN_PLOT)
-);
-
-const invalidateDotPlot = async (experimentId) => (
-  await invalidateMatchingPlots(experimentId, plots.DOT_PLOT)
-);
-
-const invalidateNormalizedMatrix = async (experimentId) => (
-  await invalidateMatchingPlots(experimentId, plots.NORMALIZED_MATRIX)
-);
-
-const invalidateVolcanoPlot = async (experimentId) => (
-  await invalidateMatchingPlots(experimentId, plots.VOLCANO_PLOT)
-);
-
-const cellSetsChangingActions = [
-  invalidateCategoricalEmbedding,
-  invalidateFrequencyPlot,
-  invalidateContinuousEmbedding,
-  invalidateMarkerHeatmapPlot,
-  invalidateCustomHeatmapPlot,
-  invalidateViolinPlot,
-  invalidateDotPlot,
-  invalidateNormalizedMatrix,
-  invalidateVolcanoPlot,
-];
-
-const configInvalidatorsByEvent = {
-  [events.CELL_SETS_MODIFIED]: async (experimentId) => (
-    // flatten because results are arrays of arrays (for each config updated for each plot type)
-    // and we want a simple array with each config updated
-    await Promise.all(
-      cellSetsChangingActions.map((func) => func(experimentId)),
-    )
-  ).flat(),
-  [events.EMBEDDING_MODIFIED]: async (experimentId) => (
-    await Promise.all([
-      invalidateTrajectoryAnalysis(experimentId),
-    ])
-  ).flat(),
+const plotsChangedByEvent = {
+  [events.CELL_SETS_MODIFIED]: [
+    plots.CATEGORICAL_EMBEDDING,
+    plots.FREQUENCY_PLOT,
+    plots.CONTINUOUS_EMBEDDING,
+    plots.MARKER_HEATMAP,
+    plots.CUSTOM_HEATMAP,
+    plots.VIOLIN_PLOT,
+    plots.DOT_PLOT,
+    plots.NORMALIZED_MATRIX,
+    plots.VOLCANO_PLOT,
+  ],
+  [events.EMBEDDING_MODIFIED]: [
+    plots.TRAJECTORY_ANALYSIS,
+  ],
 };
 
 const invalidatePlotsForEvent = async (experimentId, event, sockets) => {
   logger.log(`Invalidating for event ${event}`);
-  const updatedConfigs = await configInvalidatorsByEvent[event](experimentId);
+
+  const plotsToInvalidate = plotsChangedByEvent[event];
+
+  const updatedConfigs = (await Promise.all(
+    plotsToInvalidate.map(
+      async (plot) => await invalidateMatchingPlots(experimentId, plot),
+    ),
+    // flatten because results are arrays of arrays (for each config updated for each plot type)
+  )).flat();
 
   const update = {
     type: 'PlotConfigRefresh',
