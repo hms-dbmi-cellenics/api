e2b788e379bb94d0ab6da18bca7923a054f988e9
Anugerah Erlaut <aerlaut@live.com>
2023-01-02 13:57:01 +0000

    delete pipeline error endpoint

--- src/api.v2/routes/pipelineError.js
+++
@@ -1,7 +0,0 @@
-const { handleResponse } = require('../controllers/pipelineErrorController');
-
-module.exports = {
-  'pipelineError#response': [
-    (req, res) => handleResponse(req, res),
-  ],
-};
