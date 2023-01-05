d58dce7e0fc563159eca153a178fce315bb9df72
Alex Pickering <alexvpickering@gmail.com>
2023-01-05 12:37:52 -0800

    change admin email

--- src/utils/getAdminSub.js
+++ src/utils/getAdminSub.js
@@ -1,7 +1,7 @@
 const config = require('../config');
 const getAwsPoolId = require('../api.v2/helpers/cognito/getAwsPoolId');
 
-const adminEmail = 'admin@biomage.net';
+const adminEmail = 'cellenics_admin@listserv.med.harvard.edu';
 
 const getAdminSub = async () => {
   const userPoolId = await getAwsPoolId();
