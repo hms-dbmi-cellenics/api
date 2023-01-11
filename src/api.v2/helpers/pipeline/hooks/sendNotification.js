2a18c335db845072f16c21deec73a339f2807459
Anugerah Erlaut <aerlaut@live.com>
2023-01-11 11:40:56 +0000

    implement forgotten revert

--- src/api.v2/helpers/pipeline/hooks/sendNotification.js
+++ src/api.v2/helpers/pipeline/hooks/sendNotification.js
@@ -31,9 +31,7 @@ const sendNotification = async (message) => {
 
   const { status } = statusRes[processName];
 
-  // REMINDER: REVERT BEFORE MERGING
-  if (status === FAILED && ['staging', 'test'].includes(config.clusterEnv)) {
-  // if (status === FAILED && ['production', 'test'].includes(config.clusterEnv)) {
+  if (status === FAILED && ['production', 'test'].includes(config.clusterEnv)) {
     try {
       const { stateMachineArn } = experiment.pipelines[processName];
 
