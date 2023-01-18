146592315f5b44f61faa0990f59568faa9b2cd1b
cosa65 <martin@biomage.net>
2023-01-18 11:10:55 -0300

    Add last_gem2s_params to an experiment fields reference

--- src/api.v2/model/Experiment.js
+++ src/api.v2/model/Experiment.js
@@ -81,7 +81,7 @@ class Experiment extends BasicModel {
     }
 
     const experimentExecutionFields = [
-      'params_hash', 'state_machine_arn', 'execution_arn',
+      'params_hash', 'state_machine_arn', 'execution_arn', 'last_gem2s_params',
     ];
 
     const pipelineExecutionKeys = experimentExecutionFields.reduce((acum, current) => {
