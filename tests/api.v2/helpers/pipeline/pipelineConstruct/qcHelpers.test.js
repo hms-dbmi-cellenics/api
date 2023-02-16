cc5c6163efa6ec2fde8f8a1c33dda73c2d723194
cosa65 <martin@biomage.net>
2023-02-16 09:33:38 -0300

    Fix some tests

--- tests/api.v2/helpers/pipeline/pipelineConstruct/qcHelpers.test.js
+++ tests/api.v2/helpers/pipeline/pipelineConstruct/qcHelpers.test.js
@@ -7,113 +7,129 @@ jest.mock('../../../../../src/api.v2/helpers/s3/fileExists', () => ({
   fileExists: jest.fn(() => true),
 }));
 
-describe('helper functions for skeletons', () => {
-  it('returns the correct first step given a list', async () => {
-    const processingConfig = [
-      {
-        name: 'numGenesVsNumUmis',
-        body: {
-          auto: true,
-          filterSettings: {
-            regressionType: 'linear',
-            regressionTypeSettings: {
-              linear: {
-                'p.level': 0.001,
-              },
-              spline: {
-                'p.level': 0.001,
-              },
-            },
+const processingConfig = [
+  {
+    name: 'numGenesVsNumUmis',
+    body: {
+      auto: true,
+      filterSettings: {
+        regressionType: 'linear',
+        regressionTypeSettings: {
+          linear: {
+            'p.level': 0.001,
           },
-          enabled: true,
-          '8e6ffc70-14c1-425f-b1be-cef9656a55a5': {
-            auto: true,
-            filterSettings: {
-              regressionType: 'linear',
-              regressionTypeSettings: {
-                linear: {
-                  'p.level': 0.0002898551,
-                },
-                spline: {
-                  'p.level': 0.001,
-                },
-              },
-            },
-            defaultFilterSettings: {
-              regressionType: 'linear',
-              regressionTypeSettings: {
-                linear: {
-                  'p.level': 0.0002898551,
-                },
-                spline: {
-                  'p.level': 0.001,
-                },
-              },
-            },
-            api_url: 'http://host.docker.internal:3000',
-            enabled: true,
+          spline: {
+            'p.level': 0.001,
           },
         },
       },
-      {
-        name: 'cellSizeDistribution',
-        body: {
-          auto: true,
-          filterSettings: {
-            minCellSize: 1080,
-            binStep: 200,
-          },
-          enabled: false,
-          '8e6ffc70-14c1-425f-b1be-cef9656a55a5': {
-            auto: true,
-            filterSettings: {
-              minCellSize: 1136,
-              binStep: 200,
+      enabled: true,
+      '8e6ffc70-14c1-425f-b1be-cef9656a55a5': {
+        auto: true,
+        filterSettings: {
+          regressionType: 'linear',
+          regressionTypeSettings: {
+            linear: {
+              'p.level': 0.0002898551,
             },
-            defaultFilterSettings: {
-              minCellSize: 1136,
-              binStep: 200,
+            spline: {
+              'p.level': 0.001,
             },
-            api_url: 'http://host.docker.internal:3000',
-            enabled: false,
           },
         },
-      },
-      {
-        name: 'doubletScores',
-        body: {
-          auto: true,
-          filterSettings: {
-            probabilityThreshold: 0.5,
-            binStep: 0.05,
-          },
-          enabled: true,
-          '8e6ffc70-14c1-425f-b1be-cef9656a55a5': {
-            auto: true,
-            filterSettings: {
-              probabilityThreshold: 0.6506245,
-              binStep: 0.05,
+        defaultFilterSettings: {
+          regressionType: 'linear',
+          regressionTypeSettings: {
+            linear: {
+              'p.level': 0.0002898551,
             },
-            defaultFilterSettings: {
-              probabilityThreshold: 0.6506245,
-              binStep: 0.05,
+            spline: {
+              'p.level': 0.001,
             },
-            api_url: 'http://host.docker.internal:3000',
-            enabled: true,
           },
         },
+        api_url: 'http://host.docker.internal:3000',
+        enabled: true,
       },
-    ];
+    },
+  },
+  {
+    name: 'cellSizeDistribution',
+    body: {
+      auto: true,
+      filterSettings: {
+        minCellSize: 1080,
+        binStep: 200,
+      },
+      enabled: false,
+      '8e6ffc70-14c1-425f-b1be-cef9656a55a5': {
+        auto: true,
+        filterSettings: {
+          minCellSize: 1136,
+          binStep: 200,
+        },
+        defaultFilterSettings: {
+          minCellSize: 1136,
+          binStep: 200,
+        },
+        api_url: 'http://host.docker.internal:3000',
+        enabled: false,
+      },
+    },
+  },
+  {
+    name: 'doubletScores',
+    body: {
+      auto: true,
+      filterSettings: {
+        probabilityThreshold: 0.5,
+        binStep: 0.05,
+      },
+      enabled: true,
+      '8e6ffc70-14c1-425f-b1be-cef9656a55a5': {
+        auto: true,
+        filterSettings: {
+          probabilityThreshold: 0.6506245,
+          binStep: 0.05,
+        },
+        defaultFilterSettings: {
+          probabilityThreshold: 0.6506245,
+          binStep: 0.05,
+        },
+        api_url: 'http://host.docker.internal:3000',
+        enabled: true,
+      },
+    },
+  },
+];
 
-    const qcSteps = await getQcStepsToRun(fake.EXPERIMENT_ID, processingConfig);
+describe('helper functions for skeletons', () => {
+  it('returns the first changed step if it is before all the completed steps', async () => {
+    const completedSteps = ['ClassifierFilter'];
+
+    const qcSteps = await getQcStepsToRun(fake.EXPERIMENT_ID, processingConfig, completedSteps);
     expect(qcSteps[0]).toEqual('CellSizeDistributionFilterMap');
   });
 
-  it('returns the default first step and full state machine if the config has no updates', async () => {
-    const processingConfig = [];
+  it('returns from first not-completed step if the config has changes after that', async () => {
+    const completedSteps = [];
 
-    const qcSteps = await getQcStepsToRun(fake.EXPERIMENT_ID, processingConfig);
+    const qcSteps = await getQcStepsToRun(fake.EXPERIMENT_ID, processingConfig, completedSteps);
     expect(qcSteps[0]).toEqual('ClassifierFilterMap');
+    const stateMachine = buildQCPipelineSteps(qcSteps);
+    expect(stateMachine).toMatchSnapshot();
+  });
+
+  it('returns from first not-completed step if the config has no changes', async () => {
+    const completedSteps = [
+      'ClassifierFilter',
+      'CellSizeDistributionFilter',
+      'MitochondrialContentFilter',
+      'NumGenesVsNumUmisFilter',
+    ];
+
+    const qcSteps = await getQcStepsToRun(fake.EXPERIMENT_ID, [], completedSteps);
+    expect(qcSteps[0]).toEqual('DoubletScoresFilterMap');
     const stateMachine = buildQCPipelineSteps(qcSteps);
     expect(stateMachine).toMatchSnapshot();
   });
