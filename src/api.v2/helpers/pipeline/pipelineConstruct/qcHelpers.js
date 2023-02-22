84617e2ae768dfce60da248e8e18212b860676ac
cosa65 <martin@biomage.net>
2023-02-22 09:04:39 -0300

    Rename

--- src/api.v2/helpers/pipeline/pipelineConstruct/qcHelpers.js
+++ src/api.v2/helpers/pipeline/pipelineConstruct/qcHelpers.js
@@ -40,13 +40,13 @@ const getFirstQCStep = async (experimentId, processingConfigUpdates, backendComp
   // Choose the earliestStep by checking:
   // if pendingSteps includes it, then pendingSteps has the earliest step
   // if not, earliestChangedStep is the earliest step
-  const earliestStep = (!earliestChangedStep || pendingSteps.includes(earliestChangedStep))
+  const firstStep = (!earliestChangedStep || pendingSteps.includes(earliestChangedStep))
     ? pendingSteps[0] : earliestChangedStep;
 
   // if the earlist step to run is the first one, just return it without
   // further checks
-  if (earliestStep === qcStepNames[0]) {
-    return earliestStep;
+  if (firstStep === qcStepNames[0]) {
+    return firstStep;
   }
   // if the first step to run is not the first in the pipeline (stepNames[0])
   // then check if the experiment supports starting the pipeline from any step
@@ -55,7 +55,7 @@ const getFirstQCStep = async (experimentId, processingConfigUpdates, backendComp
   // make a more costly call to S3 to check if the file exists
   const hasCellIds = await hasFilteredCellIdsAvailable(experimentId);
   if (hasCellIds) {
-    return earliestStep;
+    return firstStep;
   }
 
 
