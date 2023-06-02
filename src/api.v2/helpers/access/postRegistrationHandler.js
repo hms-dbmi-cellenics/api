fd3a03a926d94416c6066c2587370b355f3509e7
Anugerah Erlaut <aerlaut@live.com>
2023-06-02 13:03:52 +0100

    log body and return 200

--- src/api.v2/helpers/access/postRegistrationHandler.js
+++ src/api.v2/helpers/access/postRegistrationHandler.js
@@ -1,4 +1,5 @@
 const getLogger = require('../../../utils/getLogger');
+const { OK } = require('../../../utils/responses');
 
 const UserAccess = require('../../model/UserAccess');
 
@@ -7,9 +8,13 @@ const logger = getLogger('[PostRegistrationHandler] - ');
 const postRegistrationHandler = async (req) => {
   const { userEmail, userId } = req.body;
 
+  console.log('*** req.body', req.body);
+
   new UserAccess().registerNewUserAccess(userEmail, userId);
 
   logger.log(`Post registration handled for user ${userId}`);
+
+  return OK();
 };
 
 module.exports = postRegistrationHandler;
