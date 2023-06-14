7ac24213107b39d1636ded2ad6740d1c5b77cea2
Alex Pickering <alexvpickering@gmail.com>
2023-06-14 09:51:03 -0700

    fix tests
    Signed-off-by: Alex Pickering <alexvpickering@gmail.com>

--- src/api.v2/model/ExperimentExecution.js
+++ src/api.v2/model/ExperimentExecution.js
@@ -40,7 +40,7 @@ class ExperimentExecution extends BasicModel {
         last_pipeline_params: originalRow.lastPipelineParams,
       };
 
-      if (originalRow.pipelineType === 'gem2s') {
+      if (['gem2s', 'seurat'].includes(originalRow.pipelineType)) {
         copyRow.last_pipeline_params.sampleIds = originalRow.lastPipelineParams.sampleIds
           .reduce(
             (acum, currSampleId) => {
