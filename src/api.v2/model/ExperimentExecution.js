b5badc5f6f8f7ecee5a0c9baf549aae2a3732671
cosa65 <martin@biomage.net>
2023-06-28 16:12:41 -0300

    Add comment

--- src/api.v2/model/ExperimentExecution.js
+++ src/api.v2/model/ExperimentExecution.js
@@ -40,6 +40,7 @@ class ExperimentExecution extends BasicModel {
         last_pipeline_params: originalRow.lastPipelineParams,
       };
 
+      // If it's a gem2s or seurat run, translate sample ids from lastGem2SParams
       if (['gem2s', 'seurat'].includes(originalRow.pipelineType) && ['gem2s', 'seurat'].includes(originalRow.stateMachineArn)) {
         copyRow.last_pipeline_params.sampleIds = originalRow.lastPipelineParams.sampleIds
           .reduce(
