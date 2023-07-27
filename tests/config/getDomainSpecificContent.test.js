c1e80ca07a9ff3357eea6d7c377032639036c4e1
cosa65 <martin@biomage.net>
2023-07-27 16:11:51 -0300

    Update tests in getDomainSpecificContent.test.js

--- tests/config/getDomainSpecificContent.test.js
+++ tests/config/getDomainSpecificContent.test.js
@@ -4,22 +4,31 @@ const { ACCOUNT_ID } = require('../../src/api.v2/constants');
 describe('getDomainSpecificContent tests', () => {
   // Make a snapshot for each account
   Object.keys(ACCOUNT_ID).forEach((accountId) => {
-    test(`check content for ${accountId}`, () => {
+    it(`check content for ${accountId}`, () => {
       // Set the environment variables
       process.env.AWS_ACCOUNT_ID = ACCOUNT_ID[accountId];
       process.env.NODE_ENV = 'production'; // Since the function checks if env is not 'test'
 
       const content = getDomainSpecificContent();
-      expect(content).toMatchSnapshot();
+      expect(content).toMatchSnapshot(`getDomainSpecificContent for ${accountId}`);
     });
   });
 
-  // Test for the default 'test' account
-  test('check content for the default account', () => {
-    process.env.AWS_ACCOUNT_ID = 'non_existent_account_id';
+  // Test for the default biomage_private account
+  it('check content for the biomage account', () => {
+    process.env.AWS_ACCOUNT_ID = 'some_private_account_id';
     process.env.NODE_ENV = 'production'; // Since the function checks if env is not 'test'
 
     const defaultContent = getDomainSpecificContent();
-    expect(defaultContent).toMatchSnapshot();
+    expect(defaultContent).toMatchSnapshot('getDomainSpecificContent for biomage private account');
+  });
+
+  // Test for the 'test' account
+  it('check content for the biomage account', () => {
+    process.env.AWS_ACCOUNT_ID = 'non_existent_account_id';
+    process.env.NODE_ENV = 'test'; // Since the function checks if env is not 'test'
+
+    const defaultContent = getDomainSpecificContent();
+    expect(defaultContent).toMatchSnapshot('getDomainSpecificContent for test');
   });
 });
