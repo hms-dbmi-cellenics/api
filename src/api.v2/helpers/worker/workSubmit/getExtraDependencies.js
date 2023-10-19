f2c665983ba9a2a4d808fbcd3d91e076db071da8
cosa65 <martin@biomage.net>
2023-10-19 14:44:36 -0300

    Update marker heatmap task eextra dependencies to match new shape
    Signed-off-by: cosa65 <martin@biomage.net>

--- src/api.v2/helpers/worker/workSubmit/getExtraDependencies.js
+++ src/api.v2/helpers/worker/workSubmit/getExtraDependencies.js
@@ -6,6 +6,27 @@ const getClusteringSettings = async (message) => {
   return clusteringSettings;
 };
 
+const getSelectedCellSet = async (message, body, cellSets) => {
+  const { selectedCellSet } = body.downsampleSettings;
+
+  const cellSetsKeys = cellSets
+    .find(({ key }) => key === selectedCellSet)
+    .children.map(({ key }) => key);
+
+  return cellSetsKeys;
+};
+
+const getGroupedTracksCellSets = async (message, body, cellSets) => {
+  const { groupedTracks } = body.downsampleSettings;
+
+  const cellSetsKeys = cellSets
+    .filter(({ key }) => groupedTracks.includes(key))
+    .flatMap(({ children }) => children)
+    .map(({ key }) => key);
+
+  return cellSetsKeys;
+};
+
 const dependencyGetters = {
   ClusterCells: [],
   GetExpressionCellSets: [],
@@ -19,7 +40,9 @@ const dependencyGetters = {
   GetMitochondrialContent: [],
   GetNGenes: [],
   GetNUmis: [],
-  MarkerHeatmap: [getClusteringSettings],
+  MarkerHeatmap: [
+    getClusteringSettings, getSelectedCellSet, getGroupedTracksCellSets,
+  ],
   GetTrajectoryAnalysisStartingNodes: [getClusteringSettings],
   GetTrajectoryAnalysisPseudoTime: [getClusteringSettings],
   GetNormalizedExpression: [getClusteringSettings],
@@ -28,10 +51,10 @@ const dependencyGetters = {
 
 // message is assumed to be the configureEmbedding payload received
 // from the pipeline containing clutering & embedding settings
-const getExtraDependencies = async (name, message) => {
+const getExtraDependencies = async (name, message, body, cellSets = undefined) => {
   const dependencies = await Promise.all(
     dependencyGetters[name].map(
-      (dependencyGetter) => dependencyGetter(message),
+      (dependencyGetter) => dependencyGetter(message, body, cellSets),
     ),
   );
 
