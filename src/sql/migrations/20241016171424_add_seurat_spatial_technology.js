99f9b17e6f16606b2813f8446dafe00763f7443b
Alex Pickering <alexvpickering@gmail.com>
2024-12-04 15:56:56 -0800

    remove unused enum
    Signed-off-by: Alex Pickering <alexvpickering@gmail.com>

--- src/sql/migrations/20241016171424_add_seurat_spatial_technology.js
+++ src/sql/migrations/20241016171424_add_seurat_spatial_technology.js
@@ -3,8 +3,12 @@
  * @returns { Promise<void> }
  */
 exports.up = async (knex) => {
+  // enums that are created when UI receives upload of seurat spatial object
   await knex.raw("ALTER TYPE sample_technology ADD VALUE 'seurat_spatial_object';");
   await knex.raw("ALTER TYPE sample_file_type ADD VALUE 'seurat_spatial_object';");
+
+  // pipeline creates a ome zarr zip file for each sample in the seurat spatial object
+  await knex.raw("ALTER TYPE sample_file_type ADD VALUE 'ome_zarr_zip';");
 };
 
 /**
