4e3f7fc5615da71b6ba79fee98ec25a5903a222c
Alex Pickering <alexvpickering@gmail.com>
2026-02-17 12:27:50 -0500

    fix: resolve all ESLint linting errors
    - Fixed no-param-reassign warnings in validateAndSubmitWork.js
    - Removed unused lodash import from signedUrl.js
    - Added missing newline at end of createWorkerK8s.js
    - Fixed import formatting and await-in-loop warnings in waitForAvailablePods.js
    - Refactored to use proper imports instead of global-require
    - Fixed max-len issues in migration file by extracting long SQL statements
    
    Signed-off-by: Alex Pickering <alexvpickering@gmail.com>

--- src/api.v2/helpers/worker/workSubmit/waitForAvailablePods.js
+++ src/api.v2/helpers/worker/workSubmit/waitForAvailablePods.js
@@ -1,9 +1,11 @@
+const k8s = require('@kubernetes/client-node');
 const getLogger = require('../../../../utils/getLogger');
 const asyncTimer = require('../../../../utils/asyncTimer');
+
 const logger = getLogger();
 
 const getPods = async (namespace, statusSelector, labelSelector, kc) => {
-  const k8sApi = kc.makeApiClient(require('@kubernetes/client-node').CoreV1Api);
+  const k8sApi = kc.makeApiClient(k8s.CoreV1Api);
   const pods = await k8sApi.listNamespacedPod(
     namespace, null, null, null, statusSelector, labelSelector,
   );
@@ -31,13 +33,21 @@ const getAvailablePods = async (namespace, kc, experimentId) => {
   return pods;
 };
 
-const waitForAvailablePods = async (namespace, kc, experimentId, maxWaitMs = 60000, pollIntervalMs = 1000) => {
+const waitForAvailablePods = async (
+  namespace,
+  kc,
+  experimentId,
+  maxWaitMs = 60000,
+  pollIntervalMs = 1000,
+) => {
   let pods = await getAvailablePods(namespace, kc, experimentId);
   const maxTries = Math.ceil(maxWaitMs / pollIntervalMs);
+  // eslint-disable-next-line no-await-in-loop
   for (let i = 0; pods.length < 1 && i < maxTries; i += 1) {
     logger.log('No available worker pods, waiting...');
     // eslint-disable-next-line no-await-in-loop
     await asyncTimer(pollIntervalMs);
+    // eslint-disable-next-line no-await-in-loop
     pods = await getAvailablePods(namespace, kc, experimentId);
   }
   return pods;
