19cd9529a25b8f324372ededcfa9d5e3a83d164b
Alex Pickering <alexvpickering@gmail.com>
2026-03-02 15:49:37 -0500

    add workerVersions for other funcs
    Signed-off-by: Alex Pickering <alexvpickering@gmail.com>

--- src/api.v2/helpers/worker/workerVersions.js
+++ src/api.v2/helpers/worker/workerVersions.js
@@ -1,11 +1,23 @@
 const workerVersions = {
+  ClusterCells: 1,
+  ScTypeAnnotate: 1,
+  GetExpressionCellSets: 1,
+  GetEmbedding: 1,
   ListGenes: 2,
-  MarkerHeatmap: 2,
+  DifferentialExpression: 3,
+  BatchDifferentialExpression: 1,
   GeneExpression: 2,
+  GetBackgroundExpressedGenes: 1,
+  DotPlot: 2,
+  GetDoubletScore: 1,
+  GetMitochondrialContent: 1,
+  GetNGenes: 1,
+  GetNUmis: 1,
+  MarkerHeatmap: 2,
   GetTrajectoryAnalysisStartingNodes: 2,
   GetTrajectoryAnalysisPseudoTime: 2,
-  DotPlot: 2,
-  DifferentialExpression: 3,
+  GetNormalizedExpression: 1,
+  DownloadAnnotSeuratObject: 1,
 };
 
 module.exports = workerVersions;
